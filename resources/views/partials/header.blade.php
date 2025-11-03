4eb4fd1c3e5dcc3cf97e3c25eaf1ae57830cd018
christoper santos <christopher.code.git@gmail.com>
2025-11-03 00:20:38 -0600

    Refactor header  nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,18 +11,18 @@
     <div class="mx-auto transition-all duration-500 ease-in-out" :class="scrolled ? 'max-w-4xl px-6' : 'max-w-full px-0'">
         <div
             class="border-0 border-transparent transition-all duration-500 ease-in-out [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_100ms_ease-in-out]"
-            :class="scrolled ? 'backdrop-blur-lg bg-light/5 rounded-full !border-[1px] !border-neutral-700 [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_300ms_ease-in-out_500ms]' : 'border-0 border-transparent'"
+            :class="scrolled ? 'backdrop-blur-lg bg-neutral-500/5 rounded-full !border-[1px] !border-neutral-800 [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_300ms_ease-in-out_500ms]' : 'border-0 border-transparent'"
         >
             <x-main-container>
                 <nav class="flex items-center justify-between gap-5 transition-all duration-500" :class="scrolled ? 'px-5' : ''">
                     <div>
-                        <x-app-logo-icon class="size-5 fill-current uppercase tracking-widest [filter:drop-shadow(0px_0px_15px_rgb(255_255_255_/_100%))]"/>
+                        <x-app-logo-icon class="size-5 fill-current uppercase tracking-widest [filter:drop-shadow(0px_0px_15px_rgb(255_193_7_/_100%))]"/>
                     </div>
-                    <div class="transition-all duration-500">
+                    <div class="transition-all duration-500 scale-95">
                         <flux:navbar>
-                            <flux:navbar.item href="#" icon="bolt">{{ __('Como funciona') }}</flux:navbar.item>
-                            <flux:navbar.item href="#" icon="chat-bubble-bottom-center">{{ __('Preguntas') }}</flux:navbar.item>
-                            <flux:navbar.item href="#" icon="cube">{{ __('Precios') }}</flux:navbar.item>
+                            <flux:navbar.item href="#" icon="bolt" class="!bg-transparent text-dark dark:!text-light">{{ __('Como funciona') }}</flux:navbar.item>
+                            <flux:navbar.item href="#" icon="chat-bubble-bottom-center" class="!bg-transparent text-dark dark:!text-light">{{ __('Preguntas') }}</flux:navbar.item>
+                            <flux:navbar.item href="#" icon="cube" class="!bg-transparent text-dark dark:!text-light">{{ __('Precios') }}</flux:navbar.item>
                         </flux:navbar>
                     </div>
                     <div>
