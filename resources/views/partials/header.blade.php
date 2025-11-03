06bd711a1cb04a5931910bd70bf4f7cdf05f3cdd
christoper santos <christopher.code.git@gmail.com>
2025-11-03 01:55:44 -0600

    Fix logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,13 +10,13 @@
     >
     <div class="mx-auto transition-all duration-500 ease-in-out" :class="scrolled ? 'max-w-4xl px-6' : 'max-w-full px-0'">
         <div
-            class="border-0 border-transparent transition-all duration-500 ease-in-out [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_100ms_ease-in-out]"
-            :class="scrolled ? 'backdrop-blur-lg bg-neutral-500/5 rounded-full !border-[1px] !border-neutral-800 [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_300ms_ease-in-out_500ms]' : 'border-0 border-transparent'"
+            class="border-0 border-transparent transition-all duration-500 ease-in-out [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_100ms_ease-in-out] overflow-hidden dark:overflow-visible"
+            :class="scrolled ? 'backdrop-blur-lg bg-neutral-500/5 rounded-full !border-[1px] !border-neutral-200 dark:!border-neutral-800 [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_300ms_ease-in-out_500ms]' : 'border-0 border-transparent'"
         >
             <x-main-container>
                 <nav class="flex items-center justify-between gap-5 transition-all duration-500" :class="scrolled ? 'px-5' : ''">
                     <div>
-                        <x-app-logo-icon class="fill-current uppercase tracking-widest [filter:drop-shadow(0px_0px_15px_rgb(255_193_7_/_100%))]"/>
+                        <x-app-logo-icon class="fill-current uppercase tracking-widest mr-5"/>
                     </div>
                     <div class="transition-all duration-500 scale-95">
                         <flux:navbar>
