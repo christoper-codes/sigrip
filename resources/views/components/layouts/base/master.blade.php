6d4ddcb254b8b04062bc02652343fba14403d051
christoper santos <christopher.code.git@gmail.com>
2025-11-02 19:50:34 -0600

    FEAT(client) Add guest master layout

---
+++ resources/views/components/layouts/base/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light scroll-smooth">
+    <head>
+        @include('partials.head')
+    </head>
+    <body class="relative antialiased min-h-screen bg-light text-lg text-dark dark:bg-dark dark:text-light/90 {{{ getBodyClasses() }}}">
+        {{ $slot }}
+        @include('partials.foot')
+    </body>
+</html>
---
+++ resources/views/components/layouts/guest/simple.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.base.master>
+    <main>
+        {{ $slot }}
+    </main>
+</x-layouts.base.master>
