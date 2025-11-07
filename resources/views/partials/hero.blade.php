a0ca8187e1e6e0dc1b5687b29947ce82f438ccac
christoper santos <a480120322@my.uvm.edu.mx>
2025-11-07 00:16:05 -0600

    Refactor hero

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -3,7 +3,7 @@
         <div class="min-h-screen pt-52 relative">
             <div class="absolute -left-72  -top-52 h-[380px] w-[200px] lg:h-[580px] lg:w-[400px] rounded-full blur-[120px] lg:blur-[150px] bg-yellow-50/20"></div>
             <div class="text-center flex flex-col gap-6 items-center justify-center">
-                <h1 class="text-6xl">
+                <h1 class="text-4xl md:text-6xl">
                     {{ __('Cumplimiento ') }} <span class="uppercase tracking-[5px] [filter:drop-shadow(0px_0px_15px_rgb(255_193_7_/_100%))]">{{ __('NOM-035') }}</span>
                 </h1>
                 <div class="max-w-2xl opacity-70 leading-relaxed">
