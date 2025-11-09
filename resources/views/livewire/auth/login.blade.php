4b2ed3a7c7e659aa0d65d7f57a05e32d9f79de6a
christoper santos <christopher.code.git@gmail.com>
2025-11-09 00:15:38 -0600

    Refactor login

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.auth>
     <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+        <x-auth-header :title="__('Inicia sesión en tu cuenta')" :description="__('Bienvenido de nuevo, inicia sesión para continuar')" />
 
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
@@ -11,7 +11,7 @@
             <!-- Email Address -->
             <flux:input
                 name="email"
-                :label="__('Email address')"
+                :label="__('Correo electrónico')"
                 type="email"
                 required
                 autofocus
@@ -23,27 +23,27 @@
             <div class="relative">
                 <flux:input
                     name="password"
-                    :label="__('Password')"
+                    :label="__('Contraseña')"
                     type="password"
                     required
                     autocomplete="current-password"
-                    :placeholder="__('Password')"
+                    :placeholder="__('Contraseña')"
                     viewable
                 />
 
                 @if (Route::has('password.request'))
-                    <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>
-                        {{ __('Forgot your password?') }}
+                    <flux:link class="absolute top-0 text-sm end-0 hover:!no-underline" variant="ghost" :href="route('password.request')" wire:navigate>
+                        {{ __('¿Olvidaste tu contraseña?') }}
                     </flux:link>
                 @endif
             </div>
 
             <!-- Remember Me -->
-            <flux:checkbox name="remember" :label="__('Remember me')" :checked="old('remember')" />
+            <flux:checkbox name="remember" :label="__('Recordar sesión')" :checked="old('remember')" />
 
             <div class="w-full">
                 <x-buttons.primary
-                    title="{{ __('Iniciar sesion') }}"
+                    title="{{ __('Iniciar sesión') }}"
                     type="submit"
                     class="!w-full !py-6"
                 />
@@ -52,8 +52,8 @@
 
         @if (Route::has('register'))
             <div class="space-x-1 text-sm text-center rtl:space-x-reverse text-zinc-600 dark:text-zinc-400">
-                <span>{{ __('Don\'t have an account?') }}</span>
-                <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+                <span>{{ __('¿No tienes una cuenta?') }}</span>
+                <flux:link :href="route('register')" variant="ghost" class="hover:!no-underline" wire:navigate>{{ __('Regístrate') }}</flux:link>
             </div>
         @endif
     </div>
