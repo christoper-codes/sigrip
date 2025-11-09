ffec97ee8a8ee810027b4bfc309998533d9f1125
christoper santos <christopher.code.git@gmail.com>
2025-11-08 21:56:21 -0600

    Refactor hero

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,7 +1,7 @@
 <section id="hero">
     <x-main-container>
         <div class="min-h-screen pt-52 relative">
-            <div class="absolute left-0 lg:-left-72  top-0 lg:-top-52 h-[380px] w-[200px] lg:h-[580px] lg:w-[400px] rounded-full blur-[120px] lg:blur-[150px] bg-yellow-50/20"></div>
+            <div class="absolute left-0 lg:-left-72  top-0 lg:-top-52 h-[380px] w-[200px] lg:h-[580px] lg:w-[400px] rounded-full blur-[120px] lg:blur-[150px] bg-transparent dark:bg-yellow-50/20"></div>
             <div class="text-center flex flex-col gap-6 items-center justify-center">
                 <h1 class="text-4xl md:text-6xl">
                     {{ __('Cumplimiento ') }} <span class="uppercase tracking-[5px] [filter:drop-shadow(0px_0px_15px_rgb(255_193_7_/_100%))]">{{ __('NOM-035') }}</span>
