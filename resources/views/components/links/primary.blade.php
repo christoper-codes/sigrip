85c6fe9289bea3c128ec7a63a50cabf1c1b66752
christoper santos <christopher.code.git@gmail.com>
2025-11-08 21:02:52 -0600

    Support light modo to primary button

--- resources/views/components/links/primary.blade.php
+++ resources/views/components/links/primary.blade.php
@@ -5,7 +5,7 @@
 
 <flux:link
     href="{{ $url }}"
-    {{ $attributes->merge(['class' => '!inline-block hover:!no-underline !whitespace-nowrap !px-5 !py-3 cursor-pointer! rounded-full! !text-base !bg-light hover:!bg-neutral-200 !transition-all !duration-500 !text-center !text-neutral-800']) }}
+    {{ $attributes->merge(['class' => '!inline-block hover:!no-underline !whitespace-nowrap !px-5 !py-3 cursor-pointer! rounded-full! !text-base !bg-dark dark:!bg-light hover:!bg-neutral-800 dark:hover:!bg-neutral-200 !transition-all !duration-500 !text-center text-light dark:!text-neutral-800']) }}
     variant="ghost"
     wire:navigate
     >
