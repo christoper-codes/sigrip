b43bdbd704f4e5e387cb87eae6b5a7a6813d9b44
christoper santos <christopher.code.git@gmail.com>
2025-11-08 23:58:07 -0600

    Dark appearance by default

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './components/animated-button.js';
+
+/* Appearance by default */
+(function() {
+    const preference = localStorage.getItem('flux.appearance');
+    if (!preference) localStorage.setItem('flux.appearance', 'dark');
+})();
