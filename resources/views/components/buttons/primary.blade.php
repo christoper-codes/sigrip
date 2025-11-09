7f90329d180a9d0e17104b3e7e5367a4c2c108c1
christoper santos <christopher.code.git@gmail.com>
2025-11-08 22:22:46 -0600

    Dark support to primary button

--- resources/views/components/buttons/primary.blade.php
+++ resources/views/components/buttons/primary.blade.php
@@ -4,7 +4,7 @@
 ])
 
 <flux:button
-    {{ $attributes->merge(['class' => '!whitespace-nowrap !px-5 !py-3 cursor-pointer! rounded-full! !text-base !bg-light hover:!bg-neutral-200 !transition-all !duration-500 !text-center !text-neutral-800']) }}
+    {{ $attributes->merge(['class' => '!whitespace-nowrap !px-5 !py-3 cursor-pointer! rounded-full! !text-base !bg-dark dark:!bg-light hover:!bg-neutral-800 dark:hover:!bg-neutral-200 !transition-all !duration-500 !text-center !text-white dark:!text-neutral-800']) }}
     variant="primary"
     type="{{ $type }}"
     >
