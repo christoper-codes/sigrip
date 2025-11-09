b27a4f5a6cb289f8b9b24b9a456d57249db322f8
christoper santos <christopher.code.git@gmail.com>
2025-11-08 23:39:32 -0600

    Refactor nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,32 @@
 <header
     x-data="{
         scrolled: false,
-        mobileMenuOpen: false
+        mobileMenuOpen: false,
+        checkScroll() {
+            if (window.innerWidth < 1024) {
+                this.scrolled = true
+            } else {
+                this.scrolled = window.scrollY > 50
+            }
+        }
     }"
     x-init="
-        window.addEventListener('scroll', () => {
-            scrolled = window.scrollY > 50
-        })
+        checkScroll()
+        window.addEventListener('scroll', () => checkScroll())
+        window.addEventListener('resize', () => checkScroll())
     "
     class="fixed top-0 left-0 right-0 z-50 transition-all duration-500 ease-in-out"
     :class="scrolled ? 'pt-4' : 'pt-0'"
     >
-    <div class="mx-auto transition-all duration-500 ease-in-out" :class="scrolled ? 'max-w-5xl px-6' : 'max-w-full px-0'">
+    <div
+        class="mx-auto transition-all duration-500 ease-in-out"
+        :class="scrolled ? 'max-w-5xl px-6' : 'max-w-full px-0'"
+    >
         <div
             class="border-0 border-transparent transition-all duration-500 ease-in-out [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_100ms_ease-in-out] overflow-hidden dark:overflow-visible"
-            :class="scrolled ? 'backdrop-blur-lg bg-neutral-500/5 rounded-full !border-[1px] !border-neutral-200 dark:!border-neutral-800 [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_300ms_ease-in-out_500ms]' : 'border-0 border-transparent'"
+            :class="scrolled
+                ? 'backdrop-blur-lg bg-neutral-500/5 rounded-full !border-[1px] !border-neutral-200 dark:!border-neutral-800 [transition:background-color_500ms_ease-in-out,backdrop-filter_500ms_ease-in-out,border-radius_500ms_ease-in-out,border_300ms_ease-in-out_500ms]'
+                : 'border-0 border-transparent'"
         >
             <x-main-container>
                 <nav class="flex items-center justify-between transition-all duration-500 py-2.5" :class="scrolled ? 'px-1 lg:px-5' : ''">
