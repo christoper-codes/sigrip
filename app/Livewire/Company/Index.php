816d3b224ea1e5a0fd7a99d86597b8b46ad7e954
christoper santos <christopher.code.git@gmail.com>
2025-11-12 00:36:32 -0600

    List departments

--- app/Livewire/Company/Index.php
+++ app/Livewire/Company/Index.php
@@ -3,16 +3,19 @@
 namespace App\Livewire\Company;
 
 use App\Models\Company;
+use App\Models\Department;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Index extends Component
 {
     public array $companies = [];
+    public array $departments = [];
 
     public function mount()
     {
         $this->companies = Company::where('id', Auth::user()->company->id)->get()->toArray();
+        $this->departments = Department::where('company_id', Auth::user()->company->id)->get()->toArray();
     }
 
     public function render()
--- resources/views/livewire/company/index.blade.php
+++ resources/views/livewire/company/index.blade.php
@@ -9,7 +9,7 @@
             __('Estado')
         ]"
         :searchFields="['name', 'description']"
-        :perPage="2"
+        :perPage="10"
         searchPlaceholder="{{ __('Buscar compañia...') }}"
     >
         <td class="p-4" x-text="item.name"></td>
@@ -20,4 +20,20 @@
             <x-appearance.badge status="active" />
         </td>
     </x-appearance.table>
+    <div class="mt-10">
+        <flux:heading size="lg">{{ __('Departamentos asociados') }}</flux:heading>
+        <flux:text class="mt-2">{{ __('Seleccione un departamento para ver más detalles') }}</flux:text>
+
+        <div class="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            @foreach ($departments as $department)
+                <a href="#" class="hover:scale-105 transition-transform duration-500 cursor-pointer flex flex-col gap-8 p-5 rounded-xl bg-light-variant dark:bg-dark-variant border border-neutral-300 dark:border-neutral-700">
+                    <div>
+                        <flux:heading>{{ $department['name'] }}</flux:heading>
+                        <flux:text class="mt-2">{{ $department['description'] ?? 'Sin descripción' }}</flux:text>
+                    </div>
+                    <flux:text>{{__('Administración: ')}} </flux:text>
+                </a>
+            @endforeach
+        </div>
+    </div>
 </div>
