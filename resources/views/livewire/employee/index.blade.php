7048074b02e61d3805a24ad6a36f9f43a073a35e
christoper santos <christopher.code.git@gmail.com>
2025-11-14 14:55:47 -0600

    Show employees

--- resources/views/livewire/employee/index.blade.php
+++ resources/views/livewire/employee/index.blade.php
@@ -2,39 +2,51 @@
    <form wire:submit.prevent='searchEmployees'>
         <flux:field class="max-w-md w-full">
             <flux:label>{{ __('Filtrar por departamento') }}</flux:label>
-            <flux:select class="!h-12" name="department" placeholder="{{ __('Selecciona un departamento') }}" wire:model="department">
+            <flux:select class="!h-12" name="department" wire:model.live="department">
+                <flux:select.option value="" >{{ __('Selecciona un departamento') }}</flux:select.option>
                 @foreach ($departments as $department)
                     <flux:select.option value="{{ $department['id'] }}">{{ $department['name'] }}</flux:select.option>)
                 @endforeach
             </flux:select>
-            <flux:error name="department"/>
+            <flux:error name="department" class="!mt-0"/>
         </flux:field>
         <flux:button type="submit" variant="primary" class="mt-3">{{ __('Buscar empleados') }}</flux:button>
    </form>
-   <x-appearance.table
-        :items="$employees"
-        :headers="[
-            __('Nombre'),
-            __('Email'),
-            __('Roles'),
-            __('Fecha de Creación'),
-            __('Estado'),
-            __('Aplicaciones')
-        ]"
-        :searchFields="['name']"
-        :perPage="10"
-        searchPlaceholder="{{ __('Buscar departamento...') }}"
-    >
-        <td class="p-4" x-text="item.name"></td>
-        <td class="p-4" x-text="item.email || 'Sin email'"></td>
-        <td class="p-4" x-text="item.user_roles?.map(role => role.name).join(', ') || 'Sin roles'"></td>
-        <td class="p-4" x-text="new Date(item.created_at).toLocaleDateString('es-MX')"></td>
-        <td class="p-4">
-            <x-appearance.badge status="active" />
-        </td>
-        <td class="p-4">
-            <flux:button>{{ __('Ver aplicaciones') }}</flux:button>
-        </td>
-    </x-appearance.table>
+   @if($employees)
+        <div
+            x-data="{ animation: false }"
+            x-init="$nextTick(() => animation = true)"
+            x-show="animation"
+            x-transition:enter="transition ease-out duration-500"
+            x-transition:enter-start="opacity-0 transform translate-x-8"
+            x-transition:enter-end="opacity-100 transform translate-x-0"
+            class="mt-10">
+            <x-appearance.table
+                :items="$employees"
+                :headers="[
+                    __('Nombre'),
+                    __('Email'),
+                    __('Roles'),
+                    __('Fecha de Creación'),
+                    __('Aplicaciones'),
+                    __('Estado')
+                ]"
+                :searchFields="['name']"
+                :perPage="10"
+                searchPlaceholder="{{ __('Buscar departamento...') }}"
+                >
+                <td class="p-4" x-text="item.name"></td>
+                <td class="p-4" x-text="item.email || 'Sin email'"></td>
+                <td class="p-4" x-text="item.user_roles?.map(role => role.name).join(', ') || 'Sin roles'"></td>
+                <td class="p-4" x-text="new Date(item.created_at).toLocaleDateString('es-MX')"></td>
+                <td class="p-4">
+                    <flux:link href="{{ route('dashboard') }}">{{ __('Ver aplicaciones') }}</flux:link>
+                </td>
+                <td class="p-4">
+                    <x-appearance.badge status="active" />
+                </td>
+            </x-appearance.table>
+        </div>
+    @endif
 </div>
 
