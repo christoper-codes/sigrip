0a36d1c1abfb62a05da3034dc21df3700ca37f0b
christoper santos <christopher.code.git@gmail.com>
2025-11-16 12:44:51 -0600

    Clear employee index

--- resources/views/livewire/employee/index.blade.php
+++ resources/views/livewire/employee/index.blade.php
@@ -13,49 +13,13 @@
         <flux:button type="submit" variant="primary" class="mt-3">{{ __('Buscar empleados') }}</flux:button>
    </form>
    @if($employees)
-        <div
-            x-data="{ animation: false }"
+        <div x-data="{ animation: false }"
             x-init="$nextTick(() => animation = true)"
             x-show="animation"
             x-transition:enter="transition ease-out duration-500"
             x-transition:enter-start="opacity-0 transform translate-x-8"
             x-transition:enter-end="opacity-100 transform translate-x-0"
             class="mt-10">
-            <x-appearance.table
-                :items="$employees"
-                :headers="[
-                    __('Nombre'),
-                    __('Email'),
-                    __('Roles'),
-                    __('Fecha de Creación'),
-                    __('Aplicaciones'),
-                    __('Actualizar roles')
-                ]"
-                :searchFields="['name']"
-                :perPage="10"
-                searchPlaceholder="{{ __('Buscar departamento...') }}"
-                >
-                <td class="p-4" x-text="item.name"></td>
-                <td class="p-4" x-text="item.email || 'Sin email'"></td>
-                <td class="p-4" x-text="item.user_roles?.map(role => role.name).join(', ') || 'Sin roles'"></td>
-                <td class="p-4" x-text="new Date(item.created_at).toLocaleDateString('es-MX')"></td>
-                <td class="p-4">
-                    <flux:link href="{{ route('dashboard') }}">{{ __('Ver aplicaciones') }}</flux:link>
-                </td>
-                <td class="p-4">
-                    <flux:modal.trigger name="update-roles">
-                        <flux:button>{{ __('Actualizar roles') }}</flux:button>
-                    </flux:modal.trigger>
-                    <flux:modal name="update-roles" class="md:w-96">
-                        <div>
-                            <span x-text="item.name"></span>
-                        </div>
-                    </flux:modal>
-                </td>
-            </x-appearance.table>
-        </div>
-
-        <div class="mt-10">
             <x-appearance.livewiretable
                 :headers="[
                     __('Nombre'),
