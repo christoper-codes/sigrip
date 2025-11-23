0d5744b5cfcbec7df409a1e311cadcd3c706562a
christoper santos <christopher.code.git@gmail.com>
2025-11-23 01:23:48 -0600

    Add store user module

--- app/Livewire/Employee/Store.php
+++ app/Livewire/Employee/Store.php
@@ -2,10 +2,53 @@
 
 namespace App\Livewire\Employee;
 
+use App\Models\Department;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Store extends Component
 {
+    public array $departments = [];
+
+    #[Validate(['required', 'int'])]
+    public ?int $department = null;
+
+    #[Validate(['required', 'string', 'max:255'])]
+    public ?string $name = null;
+
+    #[Validate(['required', 'email', 'max:255', 'unique:users,email'])]
+    public ?string $email = null;
+
+    #[Validate(['required', 'string', 'min:8', 'confirmed'])]
+    public ?string $password = null;
+
+    #[Validate(['required', 'string', 'min:8'])]
+    public ?string $password_confirmation = null;
+
+    public function mount()
+    {
+        $this->departments = Department::where('company_id', Auth::user()->company?->id)
+            ->get()
+            ->toArray();
+    }
+
+    public function submit(): void
+    {
+        $this->validate();
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+            'department_id' => $this->department,
+            'company_id' => Auth::user()->company?->id,
+        ]);
+
+        $this->dispatch('toast', message: __('Empleado creado correctamente.'), type: 'success');
+        $this->reset(['department', 'name', 'email', 'password', 'password_confirmation']);
+    }
+
     public function render()
     {
         return view('livewire.employee.store');
--- resources/views/livewire/employee/store.blade.php
+++ resources/views/livewire/employee/store.blade.php
@@ -1,3 +1,35 @@
 <div>
-    hey there
+    <form wire:submit.prevent="submit" class="space-y-6 px-5 py-6 lg:px-7 lg:py-7 bg-light-variant dark:bg-dark-variant border border-neutral-300 dark:border-neutral-700 rounded-xl">
+        <flux:field>
+            <flux:label>{{ __('Selecciona un departamento') }}</flux:label>
+            <flux:select class="!h-12" name="department" wire:model.live="department">
+                <flux:select.option value="" >{{ __('Selecciona un departamento') }}</flux:select.option>
+                @foreach ($departments as $department)
+                    <flux:select.option value="{{ $department['id'] }}">{{ $department['name'] }}</flux:select.option>
+                @endforeach
+            </flux:select>
+            <flux:error name="department" class="!mt-0"/>
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Nombre completo') }}</flux:label>
+            <flux:input name="name" wire:model="name" icon="briefcase" placeholder="{{ __('John Doe') }}"/>
+            <flux:error name="name" />
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Correo electrónico') }}</flux:label>
+            <flux:input type="email" name="email" wire:model="email" icon="envelope" placeholder="{{ __('john.doe@outlook.com') }}"/>
+            <flux:error name="email" />
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Contraseña') }}</flux:label>
+            <flux:input name="password" type="password" wire:model="password" icon="lock-closed" placeholder="●●●●●●●●" viewable />
+            <flux:error name="password" />
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Confirmar Contraseña') }}</flux:label>
+            <flux:input name="password_confirmation" type="password" wire:model="password_confirmation" icon="lock-closed" placeholder="●●●●●●●●" viewable />
+            <flux:error name="password_confirmation" />
+        </flux:field>
+        <flux:button type="submit" variant="primary">{{ __('Guardar') }}</flux:button>
+    </form>
 </div>
