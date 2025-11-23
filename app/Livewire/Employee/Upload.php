eaffcf351c189b8a00c4c78e929d2c038e6708e6
christoper santos <christopher.code.git@gmail.com>
2025-11-23 12:26:47 -0600

    Add download template user employee excel

--- app/Livewire/Employee/Upload.php
+++ app/Livewire/Employee/Upload.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire\Employee;
 
+use App\Exports\UsersTemplateExport;
 use Livewire\Component;
+use Maatwebsite\Excel\Facades\Excel;
 
 class Upload extends Component
 {
+
+    public function downloadTemplate()
+    {
+        return Excel::download(new UsersTemplateExport, 'neurra_users_template.xlsx');
+    }
+
     public function render()
     {
         return view('livewire.employee.upload');
--- resources/views/livewire/employee/upload.blade.php
+++ resources/views/livewire/employee/upload.blade.php
@@ -1,3 +1,5 @@
 <div>
-    hey there
+    <div wire:click='downloadTemplate' class="w-full max-w-xs flex items-center justify-center gap-3 p-5 border border-green-600 bg-green-300/10 rounded-2xl text-xs cursor-pointer hover:bg-green-300/5 transition-colors shadow-xl/50 shadow-green-500/20">
+        <p>{{ __('Descargar plantilla Excel') }}</p>
+    </div>
 </div>
