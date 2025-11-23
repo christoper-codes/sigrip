9b0232406eeb061f6f4cfcc84bcc3f92f661b082
christoper santos <christopher.code.git@gmail.com>
2025-11-23 12:35:38 -0600

    Refactor download template button

--- app/Livewire/Employee/Upload.php
+++ app/Livewire/Employee/Upload.php
@@ -5,11 +5,12 @@ namespace App\Livewire\Employee;
 use App\Exports\EmployeesTemplateExport;
 use Livewire\Component;
 use Maatwebsite\Excel\Facades\Excel;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class Upload extends Component
 {
 
-    public function downloadTemplate()
+    public function downloadTemplate(): BinaryFileResponse
     {
         return Excel::download(new EmployeesTemplateExport, 'neura_employees_template.xlsx');
     }
--- resources/views/livewire/employee/upload.blade.php
+++ resources/views/livewire/employee/upload.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <div wire:click='downloadTemplate' class="w-full max-w-xs flex items-center justify-center gap-3 p-5 border border-green-600 bg-green-300/10 rounded-2xl text-xs cursor-pointer hover:bg-green-300/5 transition-colors shadow-xl/50 shadow-green-500/20">
-        <p>{{ __('Descargar plantilla Excel') }}</p>
+    <div wire:click='downloadTemplate' class="w-full max-w-xs flex items-center justify-center gap-2 p-5 border border-green-600 bg-green-300/10 rounded-2xl text-sm cursor-pointer hover:bg-green-300/5 transition-colors shadow-xl/50 shadow-green-500/20">
+        <flux:icon.arrow-down-tray />
+        <p>{{ __('Descargar plantilla') }}</p>
     </div>
 </div>
