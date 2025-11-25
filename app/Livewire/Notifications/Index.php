ed53ee49c142001ade42b36b6437c6bf8b05ace9
Christoper Patiño <christopher.code.git@gmail.com>
2025-11-24 18:28:16 -0600

    Get user notifications

--- app/Livewire/Notifications/Index.php
+++ app/Livewire/Notifications/Index.php
@@ -2,10 +2,25 @@
 
 namespace App\Livewire\Notifications;
 
+use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public array $unread_notifications = [];
+    public array $read_notifications = [];
+
+    public function mount(): void
+    {
+        $notifications = Notification::where('user_id', Auth::user()->id)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        $this->unread_notifications = $notifications->whereNull('read_at')->toArray();
+        $this->read_notifications = $notifications->whereNotNull('read_at')->toArray();
+    }
+
     public function render()
     {
         return view('livewire.notifications.index');
