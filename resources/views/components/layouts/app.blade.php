ab0d58d10bfbafdb267d7917da4dba49ee35b7cd
Christoper Patiño <a480120322@my.uvm.edu.mx>
2025-12-06 13:01:55 -0600

    Refactor profile settings height

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main class="bg-light dark:bg-dark !mb-20">
+    <flux:main class="bg-light dark:bg-dark !mb-20 relative">
+{{--    <div class="absolute left-0 lg:-left-40  top-0 lg:-top-80 h-[380px] w-[200px] lg:h-[580px] lg:w-[400px] rounded-full blur-[120px] lg:blur-[150px] bg-transparent dark:bg-yellow-50/20"></div> --}}
+        <div class="absolute right-0 top-0 lg:-top-96 h-[380px] w-[200px] lg:h-[500px] lg:w-[400px] rounded-full blur-[120px] lg:blur-[150px] bg-transparent dark:bg-yellow-50/20"></div>
         {{ $slot }}
         <div class="flex flex-col gap-2">
             <x-app-logo-icon class="w-24"/>
--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -6,7 +6,7 @@ new class extends Component {
     //
 }; ?>
 
-<section class="w-full">
+<section class="w-full h-full">
     @include('partials.settings-heading')
 
     <x-settings.layout :heading="__('Appearance')" :subheading=" __('Update the appearance settings for your account')">
