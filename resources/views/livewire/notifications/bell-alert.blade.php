b6dde68cd5bdc5dc28b542dde41eea98c295fd9f
Christoper Patiño <a480120322@my.uvm.edu.mx>
2025-12-05 20:04:23 -0600

    Redirect to notification index

--- resources/views/livewire/notifications/bell-alert.blade.php
+++ resources/views/livewire/notifications/bell-alert.blade.php
@@ -1,8 +1,8 @@
-<div class="relative">
+<a href="{{ route('notification.index') }}" class="relative">
     <div class="flex items-center justify-center p-2 lg:p-3 rounded-full border border-neutral-300 dark:border-neutral-700 bg-light-variant dark:bg-dark-variant">
         <flux:icon.bell class="size-4 lg:size-5"/>
     </div>
     <div class="absolute -top-1 -right-1 border border-primary bg-primary/20 backdrop-blur-2xl text-xs rounded-full h-4.5 lg:h-5 w-4.5 lg:w-5 flex items-center justify-center font-semibold">
         <span>{{ $notifications }}</span>
     </div>
-</div>
+</a>
