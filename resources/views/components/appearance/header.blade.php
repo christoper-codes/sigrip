37d2b65a3f0a061f4ecc5da269f0d601b6f68309
Christoper Patiño <a480120322@my.uvm.edu.mx>
2025-12-06 13:08:48 -0600

    Add appearance in app header

--- resources/views/components/appearance/header.blade.php
+++ resources/views/components/appearance/header.blade.php
@@ -2,7 +2,11 @@
         <div>
             {{ $slot }}
         </div>
-        <div class="hidden lg:block">
+        <div class="hidden lg:flex items-center gap-x-4 relative">
+            <div x-data class="flex items-center justify-center p-2 lg:p-3 rounded-full border border-neutral-300 dark:border-neutral-700 bg-light-variant dark:bg-dark-variant">
+                <flux:icon.sun x-show="$flux.appearance === 'light'" x-on:click="$flux.dark = ! $flux.dark" class="cursor-pointer size-4! lg:size-5!" />
+                <flux:icon.moon x-show="$flux.appearance === 'dark'" x-on:click="$flux.dark = ! $flux.dark" class="cursor-pointer size-4! lg:size-5!" />
+            </div>
            <livewire:notifications.bell-alert />
         </div>
 </div>
