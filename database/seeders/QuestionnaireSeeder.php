fdde14808eec12e0625c14fd96561898fe8a9cab
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-07 11:37:58 -0600

    Add questionnaires data

--- database/seeders/QuestionnaireSeeder.php
+++ database/seeders/QuestionnaireSeeder.php
@@ -13,7 +13,12 @@ class QuestionnaireSeeder extends Seeder
     public function run(): void
     {
         Questionnaire::create([
-
+            'questionnaire_category_id' => 1,
+            'organization_id' => 1,
+            'name' => 'Escaneo emocional mensual (NOM-035)',
+            'description' => 'Cuestionario para identificar y evaluar los factores de riesgo psicosocial en el entorno laboral, conforme a la NOM-035',
+            'metadata' => [],
+            'is_active' => true,
         ]);
     }
 }
