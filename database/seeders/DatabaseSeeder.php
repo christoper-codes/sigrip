4b304a89aed066e7b4e77ee26fa233db5c97e3d9
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-07 12:10:04 -0600

    Add UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             QuestionnaireCategorySeeder::class,
             QuestionnaireSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'organization_id' => 1,
+            'name' => 'Christoper Patiño Santos',
+            'email' => 'chris@krodox.com',
+            'password' => bcrypt('12345678'),
+            'metadata' => ['notifications' => 0],
+            'is_active' => true,
+        ]);
+
+        $user->userRoles()->attach(1);
+    }
+}
