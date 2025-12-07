4bfd59cf6c784eae1a3bb50e5788d32accaba55c
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-07 11:59:05 -0600

    Add QuestionnaireSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             OrganizationSeeder::class,
             RoleSeeder::class,
             QuestionnaireCategorySeeder::class,
+            QuestionnaireSeeder::class,
         ]);
     }
 }
