543f75177e60273ae4b048ed324f7a3d6ee199ca
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-07 11:18:44 -0600

    Add questionnaire seeder

---
+++ database/seeders/QuestionnaireSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\QuestionnaireCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class QuestionnaireSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        QuestionnaireCategory::create([
+            'name' => 'Factores de riesgo psicosocial',
+            'description' => 'Evaluación para medir factores de riesgo psicosocial en el trabajo. Identificación, análisis y prevención',
+            'is_active' => true,
+        ]);
+
+        QuestionnaireCategory::create([
+            'name' => 'Bienestar y monitoreo emocional',
+            'description' => 'Evaluación para monitorear el bienestar emocional y psicológico de los empleados',
+            'is_active' => true,
+        ]);
+
+        QuestionnaireCategory::create([
+            'name' => 'Evaluación ética y honestidad',
+            'description' => 'Evaluación para medir integridad, transparencia y comportamiento ético en el entorno laboral',
+            'is_active' => true,
+        ]);
+    }
+}
