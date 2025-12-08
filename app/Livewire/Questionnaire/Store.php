c17a527ecc5e055787b38fae447799ae9b23d09e
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-08 12:03:03 -0600

    Add questionnaire input form

--- app/Livewire/Questionnaire/Store.php
+++ app/Livewire/Questionnaire/Store.php
@@ -2,10 +2,33 @@
 
 namespace App\Livewire\Questionnaire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Store extends Component
 {
+    #[Validate(['required', 'string', 'max:255'])]
+    public ?string $title = null;
+
+    #[Validate(['required', 'string', 'max:255'])]
+    public ?string $subtitle = null;
+
+    #[Validate(['required', 'string'])]
+    public ?string $instructions = null;
+
+    #[Validate(['required', 'array'])]
+    public ?array $objetives = [''];
+
+    #[Validate(['required', 'array'])]
+    public ?array $yellow_risk_evaluation = [
+        ['label' => '', 'criteria' => '']
+    ];
+
+    #[Validate(['required', 'array'])]
+    public ?array $red_risk_evaluation = [
+        ['label' => '', 'criteria' => '']
+    ];
+
     public function render()
     {
         return view('livewire.questionnaire.store');
--- resources/views/livewire/questionnaire/store.blade.php
+++ resources/views/livewire/questionnaire/store.blade.php
@@ -1,3 +1,87 @@
 <div>
+    <form wire:submit.prevent="submit" class="space-y-6 px-5 py-6 lg:px-7 lg:py-7 bg-light-variant dark:bg-dark-variant border border-neutral-300 dark:border-neutral-700 rounded-xl">
+        <flux:field>
+            <flux:label>{{ __('Titulo') }}</flux:label>
+            <flux:input name="title" wire:model="title" icon="cube" placeholder="{{ __('Plan de escaneo periodico') }}"/>
+            <flux:error name="title" />
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Subtitulo') }}</flux:label>
+            <flux:input name="subtitle" wire:model="subtitle" icon="cube-transparent" placeholder="{{ __('Evaluación de desempeño') }}"/>
+            <flux:error name="subtitle" />
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Instrucciones') }}</flux:label>
+            <flux:textarea name="instructions" resize="none" wire:model="instructions" placeholder="{{ __('Responde con sinceridad. No hay respuestas correctas o incorrectas.') }}"/>
+            <flux:error name="instructions"/>
+        </flux:field>
+         <flux:field>
+            <flux:label>{{ __('Objetivos') }}</flux:label>
+            <div class="lg:col-span-2" x-data="{ objetives: @entangle('objetives') }">
+                <div class="flex flex-col gap-2">
+                    <template x-for="(objetive, index) in objetives" :key="index">
+                        <div class="flex items-center gap-2 !max-w-2xl">
+                            <flux:input x-model="objetives[index]" placeholder="Activar alertas automáticas si aplica" icon="light-bulb"/>
+                            <flux:button icon="x-mark" icon:variant="outline" class="py-6! px-6!" x-on:click="objetives.splice(index, 1)" x-bind:disabled="objetives.length == 1" />
+                        </div>
+                    </template>
+                </div>
+                <flux:button icon="plus" variant="filled" x-on:click="objetives.push('')" class="px-4 py-4 mt-3">
+                    <span>{{ __('Agregar objetivo') }}</span>
+                </flux:button>
+            </div>
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Risk de evaluación') }}</flux:label>
+            <div>
+                <div class="flex items-center gap-x-2">
+                    <div class="size-4 bg-yellow-500 rounded"></div>
+                    <flux:text>{{ __('Evaluación de riesgo \'Yellow\'') }}</flux:text>
+                </div>
+                <div class="lg:col-span-2 mt-2 w-full" x-data="{ yellowRiskEvaluation: @entangle('yellow_risk_evaluation') }">
+                    <div class="flex flex-col gap-2 w-full">
+                        <template x-for="(risk, index) in yellowRiskEvaluation" :key="index">
+                            <div class="flex items-start gap-2 !max-w-2xl w-full">
+                                <div class="flex flex-col gap-2 w-full">
+                                    <flux:input x-model="yellowRiskEvaluation[index].label" placeholder="Riesgo moderado" icon="light-bulb" />
+                                    <flux:input x-model="yellowRiskEvaluation[index].criteria" placeholder="Promedio entre 3.0 y 3.9 o 1 respuesta crítica" icon="exclamation-triangle" />
+                                </div>
+                                <flux:button icon="x-mark" icon:variant="outline" class="py-6! px-6!" x-on:click="yellowRiskEvaluation.splice(index, 1)" x-bind:disabled="yellowRiskEvaluation.length == 1"/>
+                            </div>
+                        </template>
+                    </div>
+                    <flux:button icon="plus" variant="filled" x-on:click="yellowRiskEvaluation.push({ label: '', criteria: '' })" class="px-4 py-4 mt-3">
+                        <span>{{ __('Agregar evaluación') }}</span>
+                    </flux:button>
+                </div>
+            </div>
+        </flux:field>
+        <flux:field>
+            <flux:label>{{ __('Risk de evaluación') }}</flux:label>
+            <div>
+                <div class="flex items-center gap-x-2">
+                    <div class="size-4 bg-red-500 rounded"></div>
+                    <flux:text>{{ __('Evaluación de riesgo \'Red\'') }}</flux:text>
+                </div>
+                <div class="lg:col-span-2 mt-2 w-full" x-data="{ redRiskEvaluation: @entangle('red_risk_evaluation') }">
+                    <div class="flex flex-col gap-2 w-full">
+                        <template x-for="(risk, index) in redRiskEvaluation" :key="index">
+                            <div class="flex items-start gap-2 !max-w-2xl w-full">
+                                <div class="flex flex-col gap-2 w-full">
+                                    <flux:input x-model="redRiskEvaluation[index].label" placeholder="Riesgo moderado" icon="light-bulb" />
+                                    <flux:input x-model="redRiskEvaluation[index].criteria" placeholder="Promedio entre 3.0 y 3.9 o 1 respuesta crítica" icon="exclamation-triangle" />
+                                </div>
+                                <flux:button icon="x-mark" icon:variant="outline" class="py-6! px-6!" x-on:click="redRiskEvaluation.splice(index, 1)" x-bind:disabled="redRiskEvaluation.length == 1"/>
+                            </div>
+                        </template>
+                    </div>
+                    <flux:button icon="plus" variant="filled" x-on:click="redRiskEvaluation.push({ label: '', criteria: '' })" class="px-4 py-4 mt-3">
+                        <span>{{ __('Agregar evaluación') }}</span>
+                    </flux:button>
+                </div>
+            </div>
+        </flux:field>
 
+        <flux:button type="submit" variant="primary">{{ __('Guardar') }}</flux:button>
+    </form>
 </div>
