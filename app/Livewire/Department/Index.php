09d51cf421e991b419e3c7723b80d58fd076c11d
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-11 12:44:15 -0600

    Add edit DepartmentForm

--- app/Livewire/Department/Index.php
+++ app/Livewire/Department/Index.php
@@ -2,14 +2,20 @@
 
 namespace App\Livewire\Department;
 
+use App\Livewire\Forms\DepartmentForm;
 use App\Livewire\Traits\Table;
 use App\Models\Department;
+use App\Models\User;
+use Flux\Flux;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Index extends Component
 {
     use Table;
+
+    public DepartmentForm $form;
+    public ?Department $department;
 
     public function mount()
     {
@@ -25,9 +31,31 @@ class Index extends Component
             ['label' => __('Email')],
             ['label' => __('Descripción')],
             ['label' => __('Fecha de Creación'), 'field' => 'created_at', 'sortable' => true],
-            ['label' => __('Estado')]
+            ['label' => __('Estado')],
+            ['label' => __('Acciones')],
         ];
          $this->refreshTableData();
+    }
+
+    public function editDepartment(int $id): void
+    {
+        $this->department = Department::findOrFail($id);
+        $this->form->name = $this->department->name;
+        $this->form->email = $this->department->email;
+        $this->form->description = $this->department->description;
+        $this->form->phone = $this->department->phone;
+        if($this->department->manager_id){
+            $manager = User::find($this->department->manager_id);
+            $this->form->manager = $manager->id;
+        }
+
+         Flux::modal('edit-department-modal')->show();
+    }
+
+    public function editModalClosed()
+    {
+        $this->resetErrorBag();
+        $this->resetValidation();
     }
 
     public function render()
--- resources/views/pages/app/department/index.blade.php
+++ resources/views/pages/app/department/index.blade.php
@@ -27,8 +27,8 @@
                 </div>
                 <div x-cloak x-show="selectedTab === 'create'" id="tabpanelCreateUpdate" role="tabpanel" aria-label="create">
                     <div class="max-w-2xl">
-                        <flux:heading size="xl">{{ __('Crea tu compañía') }}</flux:heading>
-                        <flux:text class="mt-2">{{ __('Completa el formulario para configurar tu cuenta y agregar departamentos.') }}</flux:text>
+                        <flux:heading size="xl">{{ __('Crea un departamento') }}</flux:heading>
+                        <flux:text class="mt-2">{{ __('Completa el formulario para crear un nuevo departamento y asociarlo a tu compañía.') }}</flux:text>
                         <div class="mt-5">
                             <livewire:department.store :hr_department="false" />
                         </div>
