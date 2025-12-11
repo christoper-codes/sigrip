7aaccf020e98c37d0d4b2fbe347886361bc7b897
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-11 16:29:48 -0600

    Add aos library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './components/animated-button.js';
 import JSConfetti from 'js-confetti';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 window.JSConfetti = JSConfetti;
 
@@ -8,6 +10,11 @@ window.JSConfetti = JSConfetti;
     const preference = localStorage.getItem('flux.appearance');
     if (!preference) localStorage.setItem('flux.appearance', 'dark');
 })();
+
+/* Initialize AOS */
+document.addEventListener('DOMContentLoaded', () => {
+    Aos.init();
+});
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,6 +11,8 @@
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
