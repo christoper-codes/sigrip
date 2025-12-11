3741ee51706f373b063c7cd77cc09036cc0ded15
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-11 10:35:10 -0600

    Refactor app header appearance

--- resources/views/components/appearance/header.blade.php
+++ resources/views/components/appearance/header.blade.php
@@ -3,9 +3,8 @@
             {{ $slot }}
         </div>
         <div class="hidden lg:flex items-center gap-x-4 relative">
-            <div x-data class="flex items-center justify-center p-2 lg:p-3 rounded-full border border-neutral-300 dark:border-neutral-700 bg-light-variant dark:bg-dark-variant">
-                <flux:icon.sun x-show="$flux.appearance === 'light'" x-on:click="$flux.dark = ! $flux.dark" class="cursor-pointer size-4! lg:size-5!" />
-                <flux:icon.moon x-show="$flux.appearance === 'dark'" x-on:click="$flux.dark = ! $flux.dark" class="cursor-pointer size-4! lg:size-5!" />
+            <div x-data x-on:click="$flux.dark = ! $flux.dark" class="cursor-pointer flex items-center justify-center p-2 lg:p-3 rounded-full border border-neutral-300 dark:border-neutral-700 bg-light-variant dark:bg-dark-variant">
+                <flux:icon.sun class="size-4! lg:size-5!" />
             </div>
            <livewire:notifications.bell-alert />
         </div>
