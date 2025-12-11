cff4f43d82f47d856f149d6f870badab8dea00ae
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-11 10:35:42 -0600

    Refactor header app

--- resources/views/components/appearance/header.blade.php
+++ resources/views/components/appearance/header.blade.php
@@ -1,11 +1,11 @@
 <div {{ $attributes->merge(['class' => 'flex items-start justify-between mb-10']) }}>
-        <div>
-            {{ $slot }}
+    <div>
+        {{ $slot }}
+    </div>
+    <div class="hidden lg:flex items-center gap-x-4 relative">
+        <div x-data x-on:click="$flux.dark = ! $flux.dark" class="cursor-pointer flex items-center justify-center p-2 lg:p-3 rounded-full border border-neutral-300 dark:border-neutral-700 bg-light-variant dark:bg-dark-variant">
+            <flux:icon.sun class="size-4! lg:size-5!" />
         </div>
-        <div class="hidden lg:flex items-center gap-x-4 relative">
-            <div x-data x-on:click="$flux.dark = ! $flux.dark" class="cursor-pointer flex items-center justify-center p-2 lg:p-3 rounded-full border border-neutral-300 dark:border-neutral-700 bg-light-variant dark:bg-dark-variant">
-                <flux:icon.sun class="size-4! lg:size-5!" />
-            </div>
-           <livewire:notifications.bell-alert />
-        </div>
+        <livewire:notifications.bell-alert />
+    </div>
 </div>
