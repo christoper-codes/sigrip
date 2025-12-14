54c157b90b38b9a69c716eeec98ed4cf7ea057ab
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-14 15:59:29 -0600

    Get application data

--- app/Livewire/Application/Show.php
+++ app/Livewire/Application/Show.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\Application;
 
+use App\Models\Application;
 use Livewire\Component;
 
 class Show extends Component
 {
+    public Application $application;
+    public ?string $company_name = null;
+    public ?array $questionnaire = null;
+
+    public function mount(): void
+    {
+        $this->questionnaire = $this->application->questionnaire->toArray();
+        $this->company_name = $this->application->issuingDepartment->company->name;
+    }
+
     public function render()
     {
         return view('livewire.application.show');
--- resources/views/livewire/application/show.blade.php
+++ resources/views/livewire/application/show.blade.php
@@ -1,14 +1,17 @@
 <div class="relative">
     <section class="mt-10">
          <flux:text>
-            <span class="text-primary">{{ __('¡Buena tarde!') }}</span> {{ __('Christopher Patiño Santos') }}
+            <span class="text-primary">{{ __('¡Buena tarde!') }} </span>
+            @auth
+                <span> {{ auth()->user()->name }} </span>
+            @endauth
         </flux:text>
         <div class="space-y-4">
             <div class="py-2 px-4 mt-16 rounded-full text-center text-sm border bg-light-variant dark:bg-dark-variant border-neutral-300 dark:border-neutral-700 inline-block mx-auto">
-                {{ __('Victoria LLC') }}
+                {{ $company_name }}
             </div>
            <h1 class="text-4xl max-w-4xl uppercase">
-                {{ __('Escaneo emocional mensual (NOM-035) ') }}</span>
+                {{ $questionnaire['name'] }}</span>
             </h1>
             <flux:text class="mt-2">
                 {{ __('Tu opinión impulsa nuestra mejora continua.') }}
@@ -18,20 +21,17 @@
             <div class="flex items-center gap-1">
                 <flux:icon.clock variant="mini"/>
                 <flux:text>
-                    {{ __('Expiración: 03/01/2026 00:00') }}
+                    {{ __('Expiración: ') . $application['expiration_date'] }}
                 </flux:text>
             </div>
             <div class="flex flex-col lg:flex-row lg:flex-wrap lg:items-center justify-start gap-3 max-w-4xl">
-                <div class="py-2 px-4 rounded-full text-center text-sm border bg-light-variant dark:bg-dark-variant border-neutral-300 dark:border-neutral-700 w-max">
-                    <span class="inline">
-                        {{__('Responde con sinceridad')}}
-                    </span>
-                </div>
-                <div class="py-2 px-4 rounded-full text-center text-sm border bg-light-variant dark:bg-dark-variant border-neutral-300 dark:border-neutral-700 w-max">
-                    <span class="inline">
-                        {{__('No hay respuestas correctas o incorrectas')}}
-                    </span>
-                </div>
+                @foreach ($questionnaire['instructions'] as $instruction)
+                    <div class="py-2 px-4 rounded-full text-center text-sm border bg-light-variant dark:bg-dark-variant border-neutral-300 dark:border-neutral-700 w-max">
+                        <span class="inline">
+                            {{ __($instruction) }}
+                        </span>
+                    </div>
+                @endforeach
             </div>
         </div>
     </section>
