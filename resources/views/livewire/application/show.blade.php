d858aec7479eb7ea2a9c73c25ba4275e4bf79aae
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-14 14:41:28 -0600

    Add show application

---
+++ resources/views/livewire/application/show.blade.php
@@ -0,0 +1,38 @@
+<div>
+    <section class="mt-10">
+         <flux:text>
+            <span class="text-primary">{{ __('¡Buena tarde!') }}</span> {{ __('Christopher Patiño Santos') }}
+        </flux:text>
+        <div class="space-y-4">
+            <div class="py-2 px-4 mt-16 rounded-full text-center text-sm border bg-light-variant dark:bg-dark-variant border-neutral-300 dark:border-neutral-700 inline-block mx-auto">
+                {{ __('Victoria LLC') }}
+            </div>
+           <h1 class="text-4xl max-w-4xl uppercase">
+                {{ __('Escaneo emocional mensual (NOM-035) ') }}</span>
+            </h1>
+            <flux:text class="mt-2">
+                {{ __('Tu opinión impulsa nuestra mejora continua.') }}
+            </flux:text>
+        </div>
+        <div class="space-y-4 mt-16">
+            <div class="flex items-center gap-1">
+                <flux:icon.clock class="text-primary!" variant="mini"/>
+                <flux:text>
+                    {{ __('Expiración: 03/01/2026 00:00') }}
+                </flux:text>
+            </div>
+            <div class="flex flex-col lg:flex-row lg:flex-wrap lg:items-center justify-start gap-3 max-w-4xl">
+                <div class="py-2 px-4 rounded-full text-center text-sm border bg-light-variant dark:bg-dark-variant border-neutral-300 dark:border-neutral-700 w-max">
+                    <span class="inline">
+                        {{__('Responde con sinceridad')}}
+                    </span>
+                </div>
+                <div class="py-2 px-4 rounded-full text-center text-sm border bg-light-variant dark:bg-dark-variant border-neutral-300 dark:border-neutral-700 w-max">
+                    <span class="inline">
+                        {{__('No hay respuestas correctas o incorrectas')}}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- resources/views/pages/app/application/show.blade.php
+++ resources/views/pages/app/application/show.blade.php
@@ -1,4 +1,16 @@
 <x-layouts.application :title="__('Applicación')">
-
-
+    <section id="application-show">
+        <x-main-container>
+                <div class="flex items-center justify-between">
+                    <a href="{{ route('home') }}" wire:navigate>
+                        <x-app-logo-icon class="w-24"/>
+                    </a>
+                <div x-data class="size-7 border border-neutral-300 dark:border-neutral-600 rounded-full flex items-center justify-center">
+                        <flux:icon.sun x-show="$flux.appearance === 'light'" x-on:click="$flux.dark = ! $flux.dark" variant="mini" class="cursor-pointer size-4!" />
+                        <flux:icon.sun x-show="$flux.appearance === 'dark'" x-on:click="$flux.dark = ! $flux.dark" variant="mini" class="cursor-pointer size-4!" />
+                    </div>
+                </div>
+            <livewire:application.show :application="$application" />
+        </x-main-container>
+    </section>
 </x-layouts.application>
