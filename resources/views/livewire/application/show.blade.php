009f2fdd8208a52caee8973a2753d201fea69b9f
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-14 15:04:59 -0600

    Add questionaries view in application show

--- resources/views/livewire/application/show.blade.php
+++ resources/views/livewire/application/show.blade.php
@@ -16,7 +16,7 @@
         </div>
         <div class="space-y-4 mt-16">
             <div class="flex items-center gap-1">
-                <flux:icon.clock class="text-primary!" variant="mini"/>
+                <flux:icon.clock variant="mini"/>
                 <flux:text>
                     {{ __('Expiración: 03/01/2026 00:00') }}
                 </flux:text>
@@ -32,6 +32,36 @@
                         {{__('No hay respuestas correctas o incorrectas')}}
                     </span>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section class="mt-20">
+        <div>
+            <div class="p-5 rounded-2xl border border-light-variant dark:border-dark-variant bg-light-variant dark:bg-dark-variant">
+                <flux:heading size="lg" class="text-primary">{{ __('Escaneo general') }}</flux:heading>
+                <flux:text class="mt-2">{{ __('Evaluación del estado emocional general y factores básicos de bienestar laboral') }}</flux:text>
+            </div>
+            <div class="mt-7 flex flex-col gap-10">
+                <flux:field class="w-full max-w-3xl">
+                    <flux:label>{{ __('¿Cómo te sentiste en general este mes?') }}</flux:label>
+                    <flux:radio.group name="" class="mt-2 ml-1">
+                        <flux:radio value="1" label="Muy mal" />
+                        <flux:radio value="2" label="Mal" />
+                        <flux:radio value="3" label="Neutral" />
+                        <flux:radio value="4" label="Bien" />
+                        <flux:radio value="5" label="Muy bien" />
+                    </flux:radio.group>
+                    <flux:error name="" class="!mt-0"/>
+                </flux:field>
+                <flux:field class="w-full max-w-3xl">
+                    <flux:label>{{ __('¿Te has sentido estresado/a en el trabajo este mes?') }}</flux:label>
+                    <flux:radio.group name="" class="mt-2 ml-1">
+                        <flux:radio value="1" label="Sí, constantemente" />
+                        <flux:radio value="2" label="Sí, a veces " />
+                        <flux:radio value="3" label="No" />
+                    </flux:radio.group>
+                    <flux:error name="" class="!mt-0"/>
+                </flux:field>
             </div>
         </div>
     </section>
--- resources/views/pages/app/application/show.blade.php
+++ resources/views/pages/app/application/show.blade.php
@@ -6,9 +6,9 @@
                         <x-app-logo-icon class="w-24"/>
                     </a>
                 <div x-data class="size-7 border border-neutral-300 dark:border-neutral-600 rounded-full flex items-center justify-center">
-                        <flux:icon.sun x-show="$flux.appearance === 'light'" x-on:click="$flux.dark = ! $flux.dark" variant="mini" class="cursor-pointer size-4!" />
-                        <flux:icon.sun x-show="$flux.appearance === 'dark'" x-on:click="$flux.dark = ! $flux.dark" variant="mini" class="cursor-pointer size-4!" />
-                    </div>
+                    <flux:icon.sun x-show="$flux.appearance === 'light'" x-on:click="$flux.dark = ! $flux.dark" variant="mini" class="cursor-pointer size-4!" />
+                    <flux:icon.sun x-show="$flux.appearance === 'dark'" x-on:click="$flux.dark = ! $flux.dark" variant="mini" class="cursor-pointer size-4!" />
+                </div>
                 </div>
             <livewire:application.show :application="$application" />
         </x-main-container>
