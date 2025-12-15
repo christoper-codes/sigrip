384ecef6fe49630b4ef6b84fccdf90400ba9b063
sandra santos <sandrapatiho@gmail.com>
2025-12-15 16:08:00 -0600

    Add  component for rights reserved

---
+++ resources/views/components/appearance/rightsreserved.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-col gap-2">
+    <x-app-logo-icon class="w-24"/>
+    <p class="text-sm">© {{ date('Y') }} {{ __('NEURA. Todos los derechos reservados.') }}</p>
+    <p class="text-xs opacity-70">{{ __('NEURA® es una marca registrada.') }}</p>
+</div>
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,10 +3,6 @@
 {{--    <div class="absolute left-0 lg:-left-40  top-0 lg:-top-80 h-[380px] w-[200px] lg:h-[580px] lg:w-[400px] rounded-full blur-[120px] lg:blur-[150px] bg-transparent dark:bg-yellow-50/20"></div> --}}
         <div class="absolute right-0 top-0 lg:-top-96 h-[380px] w-[200px] lg:h-[500px] lg:w-[400px] rounded-full blur-[120px] lg:blur-[150px] bg-transparent dark:bg-yellow-50/20"></div>
         {{ $slot }}
-        <div class="flex flex-col gap-2">
-            <x-app-logo-icon class="w-24"/>
-            <p class="text-sm">© {{ date('Y') }} {{ __('NEURA. Todos los derechos reservados.') }} </p>
-            <p class="text-xs opacity-70">{{ __('NEURA® es una marca registrada.') }}</p>
-        </div>
+        <x-appearance.rightsreserved />
     </flux:main>
 </x-layouts.app.sidebar>
