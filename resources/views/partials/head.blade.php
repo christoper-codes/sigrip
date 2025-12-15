d565d2e1d7a38d20cf504baf92142a87d65c5961
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-14 21:03:17 -0600

    Add logo favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,13 +5,14 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" href="/favicon.svg" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
 
