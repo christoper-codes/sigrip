d65a409ad1a2e4ea7b028db8bd7257dd5ed952d8
sandra santos <sandrapatiho@gmail.com>
2025-12-15 15:19:51 -0600

    Add fix footer to show real year

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
         {{ $slot }}
         <div class="flex flex-col gap-2">
             <x-app-logo-icon class="w-24"/>
-            <p class="text-sm">{{ __('© 2025 NEURA. Todos los derechos reservados.') }}</p>
+            <p class="text-sm">© {{ date('Y') }} {{ __('NEURA. Todos los derechos reservados.') }} </p>
             <p class="text-xs opacity-70">{{ __('NEURA® es una marca registrada.') }}</p>
         </div>
     </flux:main>
--- resources/views/pages/app/application/show.blade.php
+++ resources/views/pages/app/application/show.blade.php
@@ -15,7 +15,7 @@
             <livewire:application.show :application="$application" />
             <div class="flex flex-col gap-2 mt-40">
                 <x-app-logo-icon class="w-24"/>
-                <p class="text-sm">{{ __('© 2025 NEURA. Todos los derechos reservados.') }}</p>
+                <p class="text-sm">© {{ date('Y') }} {{ __('NEURA. Todos los derechos reservados.') }}</p>
                 <p class="text-xs opacity-70">{{ __('NEURA® es una marca registrada.') }}</p>
             </div>
         </flux:main>
