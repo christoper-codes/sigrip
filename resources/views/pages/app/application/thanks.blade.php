cd702f91df0735669c092082816f9e7d75178aea
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-15 16:21:50 -0600

    Use rightsreserved component

--- resources/views/pages/app/application/thanks.blade.php
+++ resources/views/pages/app/application/thanks.blade.php
@@ -13,11 +13,7 @@
                 </div>
             </div>
             <livewire:application.thanks />
-            <div class="flex flex-col gap-2">
-                <x-app-logo-icon class="w-24"/>
-                <p class="text-sm">{{ __('© 2025 NEURA. Todos los derechos reservados.') }}</p>
-                <p class="text-xs opacity-70">{{ __('NEURA® es una marca registrada.') }}</p>
-            </div>
+            <x-appearance.rightsreserved />
         </flux:main>
     </section>
 </x-layouts.application>
