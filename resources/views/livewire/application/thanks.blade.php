d87e2cf0b223ee4b622f86c72ef85e99e64635a7
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-15 16:33:11 -0600

    Add thanks livewire component

--- resources/views/livewire/application/thanks.blade.php
+++ resources/views/livewire/application/thanks.blade.php
@@ -1,3 +1,9 @@
-<div>
-    hey there thanks page !
+<div class="text-center flex items-center justify-center">
+    <div>
+        <flux:icon.check-circle class="size-16! text-green-500 mx-auto mb-6" />
+         <flux:heading size="xl" class="uppercase!">{{ __('¡Gracias por contestar el formulario!') }}</flux:heading>
+        <flux:text class="mt-4 mb-8">
+            {{ __('Hemos recibido tus respuestas correctamente. Tu opinión impulsa nuestra mejora continua.') }}
+        </flux:text>
+    </div>
 </div>
