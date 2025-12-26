d1a57d7c24f9a387c1d7b0666b0072a08af2b1de
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-26 16:35:23 -0600

    Show store ticket fields

--- app/Livewire/Ticket/Store.php
+++ app/Livewire/Ticket/Store.php
@@ -3,19 +3,39 @@
 namespace App\Livewire\Ticket;
 
 use App\Enums\NotificationTypesEnum;
+use App\Models\IncidentType;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Store extends Component
 {
+    public ?array $departments = [];
+    public ?array $incident_types = [];
+    public bool $is_priority = true;
+    public bool $is_anonymous = false;
+
+    #[Validate(['required', 'integer'])]
+    public ?int $department = null;
+
+    #[Validate(['required', 'integer'])]
+    public ?int $incident_type = null;
+
+    #[Validate(['required', 'string', 'min:3', 'max:255'])]
+    public ?string $title = null;
+
+    #[Validate(['required', 'string', 'min:3'])]
+    public ?string $description = null;
 
     public function mount(): void
     {
+        $this->departments = Auth::user()->company?->departments->toArray() ?? [];
+        $this->incident_types = IncidentType::all()->toArray();
     }
 
     public function submit(): void
     {
         $this->validate();
-
 
         $this->dispatch('toast', message: __('Ticket creado correctamente.'), type: NotificationTypesEnum::SUCCESS->value);
         $this->reset();
--- resources/views/livewire/ticket/store.blade.php
+++ resources/views/livewire/ticket/store.blade.php
@@ -1,102 +1,43 @@
 <div>
     <form wire:submit.prevent="submit" class="space-y-6 px-5 py-6 lg:px-7 lg:py-7 bg-light-variant dark:bg-dark-variant border border-neutral-300 dark:border-neutral-700 rounded-xl">
         <flux:field>
-            <flux:label>{{ __('Nombre') }}</flux:label>
-            <flux:input name="name" wire:model="form.name" icon="users" placeholder="{{ __('Recursos Humanos') }}"/>
-            <flux:error name="form.name" />
+            <flux:label>{{ __('Selecciona un departamento') }}</flux:label>
+            <flux:select class="!h-12" name="department" wire:model.live="department">
+                <flux:select.option value="" >{{ __('Selecciona un departamento') }}</flux:select.option>
+                @foreach ($departments as $department)
+                    <flux:select.option value="{{ $department['id'] }}">{{ $department['name'] }}</flux:select.option>
+                @endforeach
+            </flux:select>
+            <flux:error name="department" class="!mt-0"/>
         </flux:field>
         <flux:field>
-            <flux:label>{{ __('Email') }}</flux:label>
-            <flux:input name="email" wire:model="form.email" icon="envelope" placeholder="{{ __('hello@neura.com') }}"/>
-            <flux:error name="form.email" />
+            <flux:label>{{ __('Selecciona un tipo de incidente') }}</flux:label>
+            <flux:select class="!h-12" name="incident_type" wire:model.live="incident_type">
+                <flux:select.option value="" >{{ __('Selecciona un tipo de incidente') }}</flux:select.option>
+                @foreach ($incident_types as $incident_type)
+                    <flux:select.option value="{{ $incident_type['id'] }}">{{ $incident_type['name'] }}</flux:select.option>
+                @endforeach
+            </flux:select>
+            <flux:error name="incident_type" class="!mt-0"/>
         </flux:field>
         <flux:field>
-            <flux:label>{{ __('Teléfono') }}</flux:label>
-            <flux:input name="phone" wire:model="form.phone" mask="(999) 999-9999" icon="phone" placeholder="{{ __('(555) 555-5555') }}"/>
-            <flux:error name="form.phone" />
+            <flux:label>{{ __('Incidencia') }}</flux:label>
+            <flux:input name="title" wire:model="title" icon="exclamation-triangle" placeholder="{{ __('Título de la incidencia') }}"/>
+            <flux:error name="title" />
         </flux:field>
         <flux:field>
-            <flux:label>{{ __('Descripción') }}</flux:label>
-            <flux:textarea name="description" resize="none" wire:model="form.description" icon="chat-bubble-bottom-center-text" placeholder="{{ __('Departamento de recursos humanos') }}"/>
-            <flux:error name="form.description"/>
+            <flux:label>{{ __('Descripción de la incidencia') }}</flux:label>
+            <flux:textarea name="description" resize="none" wire:model="description" icon="chat-bubble-bottom-center-text" placeholder="{{ __('Descripción detallada') }}"/>
+            <flux:error name="description"/>
         </flux:field>
         <flux:field>
-            <div class="flex items-start gap-2 mt-2">
-                <flux:icon.chat-bubble-oval-left />
-                <flux:text>{{ __('Esta sección es opcional') }}</flux:text>
-            </div>
-            <flux:modal.trigger name="manage-manager">
-                <flux:button class="py-6">{{ __('Gerente del departamento') }}</flux:button>
-            </flux:modal.trigger>
+            <flux:switch label="Es prioridad" wire:model="is_priority" align="left" name="is_priority"/>
+            <flux:error name="is_priority" />
         </flux:field>
         <flux:field>
-            <flux:switch label="Es departamento de RH" wire:model="form.hr_department" align="left" name="hr_department"/>
-            <flux:error name="form.hr_department" />
+            <flux:switch label="Es anónimo" wire:model="is_anonymous" align="left" name="is_anonymous"/>
+            <flux:error name="is_anonymous" />
         </flux:field>
-        <flux:button type="submit" variant="primary">{{ __('Guardar') }}</flux:button>
+        <flux:button type="submit" variant="primary">{{ __('Crear ticket') }}</flux:button>
     </form>
-    <flux:modal name="manage-manager" class="w-[90%] md:w-xl">
-        <div>
-            <flux:heading size="lg">{{ __('Actualizar gerente') }}</flux:heading>
-            <div class="flex items-start gap-2 mt-2">
-                <flux:icon.chat-bubble-oval-left-ellipsis />
-                <flux:text>{{ __('Busque y seleccione un gerente para el departamento.') }}</flux:text>
-            </div>
-            <div class="mt-5 space-y-3">
-                <div>
-                    <form wire:submit.prevent='searchManager'>
-                        <flux:label>{{ __('Buscar y asignar gerente') }}</flux:label>
-                        <flux:input name="search_manager" icon="magnifying-glass" placeholder="{{ __('Nombre o email') }}" class="mt-1" wire:model="form.search_manager" autocomplete="off"/>
-                        <flux:error name="form.search_manager" />
-                        <flux:button type="submit" class="mt-3">{{ __('buscar') }}</flux:button>
-                    </form>
-
-                @if(isset($form->potential_managers) && $form->potential_managers && $form->potential_managers->isNotEmpty())
-                        <div
-                            x-data="{
-                                selected: @entangle('form.manager'),
-                                animation: false,
-                                toggle(id) {
-                                    if (this.selected === id) {
-                                        this.selected = null;
-                                    } else {
-                                        this.selected = id;
-                                    }
-                                }
-                            }"
-                            x-init="$nextTick(() => animation = true)"
-                            x-show="animation"
-                            x-transition
-                            >
-                            <flux:checkbox.group class="mt-5">
-                                @foreach ($form->potential_managers as $manager)
-                                    <flux:checkbox
-                                        value="{{ $manager->id }}"
-                                        label="{{ $manager->name }}"
-                                        x-bind:checked="selected === {{ $manager->id }}"
-                                        description="{{ $manager->email }} - {{ $manager->userRoles->pluck('name')->join(', ') }}"
-                                        @click="toggle({{ $manager->id }})"
-                                    />
-                                @endforeach
-                            </flux:checkbox.group>
-                        </div>
-                    @endif
-                    <div class="mt-5 flex justify-end items-center gap-2">
-                        <flux:modal.close>
-                            <flux:button>{{ __('Cancelar') }}</flux:button>
-                        </flux:modal.close>
-                        <flux:modal.close>
-                            <flux:button variant="primary" wire:click="$set('form.save_manager', true)">{{ __('Guardar') }}</flux:button>
-                        </flux:modal.close>
-                    </div>
-                </div>
-                <div class="my-5">
-                    <flux:separator text="Or" />
-                </div>
-                <a href="{{ route('employee.index') }}" wire:navigate class="block w-full p-5  border border-neutral-300 dark:border-neutral-700 rounded-lg text-center">
-                    <flux:text>{{ __('Actualizar un empleado con rol de gerente') }}</flux:text>
-                </a>
-            </div>
-        </div>
-    </flux:modal>
 </div>
