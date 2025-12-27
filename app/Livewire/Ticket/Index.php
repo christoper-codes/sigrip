1080d45cf26255325162cd7eb00f2525265cc90f
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-26 23:52:30 -0600

    Clean index tickets

--- app/Livewire/Ticket/Index.php
+++ app/Livewire/Ticket/Index.php
@@ -2,17 +2,12 @@
 
 namespace App\Livewire\Ticket;
 
-use App\Enums\NotificationTypesEnum;
-use App\Jobs\SupportTicketJob;
-use App\Models\IncidentType;
-use App\Models\SupportTicket;
-use App\Models\SupportTicketStatus;
-use Illuminate\Support\Facades\Auth;
-use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public ?array $tickets = [];
+
     public function mount(): void
     {
 
