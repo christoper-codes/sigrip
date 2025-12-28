7ab843a20cecaa12b2ed0bb5f5d9a003244af281
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-28 16:35:58 -0600

    Refactor sidebar app

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -41,13 +41,16 @@
                     :expanded="
                         request()->routeIs('notification.index') ||
                         request()->routeIs('alert.index') ||
-                        request()->routeIs('ticket.index')
+                        request()->routeIs('ticket.index') ||
+                        auth()->user()->metadata['notifications'] > 0 ||
+                        auth()->user()->metadata['alerts'] > 0 ||
+                        auth()->user()?->company?->getActiveTickets() > 0
                     "
                     :heading="__('Primordial')" class="grid">
                     <flux:sidebar.item class="!py-5 cursor-pointer! text-neutral-300! dark:text-neutral-200!" icon="bell" :href="route('notification.index')" :current="request()->routeIs('notification.index')" wire:navigate>
                         <span>{{ __('Notificaciones') }}</span>
                         @if(auth()->user()->metadata['notifications'] > 0)
-                            <div class="inline rounded-sm border border-primary bg-primary/10 text-center text-xs px-2 py-0.5 ml-1">
+                            <div class="inline rounded-sm border border-blue-500 bg-blue-500/10 text-center text-xs px-2 py-0.5 ml-1">
                                 {{ auth()->user()->metadata['notifications'] }}
                             </div>
                         @endif
@@ -55,7 +58,7 @@
                     <flux:sidebar.item class="!py-5 cursor-pointer! text-neutral-300! dark:text-neutral-200!" icon="exclamation-triangle" :href="route('alert.index')" :current="request()->routeIs('alert.index')" wire:navigate>
                         <span>{{ __('Alertas') }}</span>
                         @if(auth()->user()->metadata['alerts'] > 0)
-                            <div class="inline rounded-sm border border-blue-500 bg-blue-500/10 text-center text-xs px-2 py-0.5 ml-1">
+                            <div class="inline rounded-sm border border-primary bg-primary/10 text-center text-xs px-2 py-0.5 ml-1">
                                 {{ auth()->user()->metadata['alerts'] }}
                             </div>
                         @endif
@@ -63,7 +66,7 @@
                     <flux:sidebar.item class="!py-5 cursor-pointer! text-neutral-300! dark:text-neutral-200!" icon="document-text" :href="route('ticket.index')" :current="request()->routeIs('ticket.index')" wire:navigate>
                         <span>{{ __('Tickets') }}</span>
                         @if(auth()->user()?->company?->getActiveTickets() > 0)
-                            <div class="inline rounded-sm border border-red-500 bg-red-500/10 text-center text-xs px-2 py-0.5 ml-1">
+                            <div class="inline rounded-sm border border-primary bg-primary/10 text-center text-xs px-2 py-0.5 ml-1">
                                 {{ auth()->user()->company->getActiveTickets() }}
                             </div>
                         @endif
