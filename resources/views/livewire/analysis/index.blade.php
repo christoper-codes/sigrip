8377d39208aa221307ed55a71ea6c147b1b92582
Christoper Patiño <christopher.code.git@gmail.com>
2025-12-31 21:37:40 -0600

    Add charts css custom file

--- resources/views/livewire/analysis/index.blade.php
+++ resources/views/livewire/analysis/index.blade.php
@@ -1,6 +1,6 @@
-<div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+<div class="grid grid-cols-1 md:grid-cols-2 gap-14">
     <!-- 2. Distribución de estados de aplicaciones (pastel) -->
-    <div class="flex flex-col gap-5 text-center">
+    <div class="flex flex-col gap-5 text-center h-72">
         <flux:heading>{{ __('Distribución de estados de aplicaciones') }}</flux:heading>
         <livewire:livewire-pie-chart
             key="{{ $pieChartModelStates->reactiveKey() }}"
@@ -9,7 +9,7 @@
     </div>
 
     <!-- 4. Alertas generadas por nivel de riesgo (pastel) -->
-    <div class="flex flex-col gap-5 text-center">
+    <div class="flex flex-col gap-5 text-center h-72">
         <flux:heading>{{ __('Alertas por nivel de riesgo') }}</flux:heading>
         <livewire:livewire-pie-chart
             key="{{ $pieChartModelAlerts->reactiveKey() }}"
@@ -18,7 +18,7 @@
     </div>
 
     <!-- 1. Total de respuestas por aplicación (barras) -->
-    <div class="col-span-1 md:col-span-2 flex flex-col gap-5 text-center">
+    <div class="col-span-1 md:col-span-2 flex flex-col gap-5 text-center h-96">
         <flux:heading>{{ __('Total de respuestas por aplicación') }}</flux:heading>
         <livewire:livewire-column-chart
             key="{{ $columnChartModel->reactiveKey() }}"
@@ -27,7 +27,7 @@
     </div>
 
     <!-- 3. Top cuestionarios más respondidos (barras horizontales) -->
-    <div class="col-span-1 md:col-span-2 flex flex-col gap-5 text-center">
+    <div class="col-span-1 md:col-span-2 flex flex-col gap-5 text-center h-96">
         <flux:heading>{{ __('Top cuestionarios más respondidos') }}</flux:heading>
         <livewire:livewire-column-chart
             key="{{ $columnChartModelTop->reactiveKey() }}"
@@ -36,7 +36,7 @@
     </div>
 
     <!-- 5. Evolución de alertas críticas (rojas) en el tiempo (líneas) -->
-    <div class="col-span-1 md:col-span-2 flex flex-col gap-5 text-center">
+    <div class="col-span-1 md:col-span-2 flex flex-col gap-5 text-center h-80">
         <flux:heading>{{ __('Evolución de alertas críticas (rojas) en el mes') }}</flux:heading>
         <livewire:livewire-line-chart
             key="{{ $lineChartModel->reactiveKey() }}"
