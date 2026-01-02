fe05b3659f253a9f59312c6267835bae717a9497
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-01 18:28:59 -0600

    Back test changes

--- app/Livewire/Employee/Index.php
+++ app/Livewire/Employee/Index.php
@@ -20,16 +20,31 @@ class Index extends Component
     use LimitItems;
 
     public array $departments = [];
+    public ?string $employee_name = null;
     public bool $search_employees = false;
 
     #[Validate(['required', 'int'])]
     public ?int $department = null;
 
-    public function mount(): void
+    public function mount()
     {
         $this->departments = Department::where('company_id', Auth::user()->company?->id)
             ->get()
             ->toArray();
+        if(! $this->departments){
+          $this->dispatch('toast', message: __('No hay departamentos disponibles.'), type: 'warning');
+        }
+
+        $this->search_fields = ['name', 'email'];
+        $this->headers = [
+            ['label' => __('Nombre'), 'field' => 'name', 'sortable' => true],
+            ['label' => __('Email'), 'field' => 'email', 'sortable' => true],
+            ['label' => __('Roles')],
+            ['label' => __('Fecha de Creación'), 'field' => 'created_at', 'sortable' => true],
+            ['label' => __('Ver aplicaciones')],
+            ['label' => __('Actualizar roles')],
+            ['label' => __('Eliminar')],
+        ];
     }
 
     public function searchEmployees(): void
@@ -47,6 +62,55 @@ class Index extends Component
         $this->search_query = '';
         $this->refreshTableData();
         $this->search_employees = true;
+    }
+
+    public function openRoleModal(int $employee_id): void
+    {
+        $this->selected_employee_id = $employee_id;
+        $this->loadRoles($employee_id);
+
+        Flux::modal('update-roles-modal')->show();
+    }
+
+    public function updateEmployeeRoles(): void
+    {
+        $this->updateRoles();
+        $this->table_items = collect($this->table_items)->map(function ($item) {
+            if ($item['id'] === $this->selected_employee_id) {
+                $item['user_roles'] = User::find($this->selected_employee_id)->userRoles->toArray();
+            }
+            return $item;
+        })->toArray();
+
+        $this->refreshTableData();
+
+        Flux::modal('update-roles-modal')->close();
+    }
+
+    public function confirmDestroy(int $employee_id, string $employee_name): void
+    {
+        $this->selected_employee_id = $employee_id;
+        $this->employee_name = $employee_name;
+        Flux::modal('confirm-destroy-employee-modal')->show();
+    }
+
+    public function destroyEmployee(): void
+    {
+        $employee = User::find($this->selected_employee_id);
+        $system_owner = $employee->hasRole(RoleEnum::SYSTEM_OWNER->value);
+        $company_admin = Auth::user()->hasRole(RoleEnum::COMPANY_ADMIN->value);
+        if ($system_owner || $company_admin) {
+            Flux::modal('confirm-destroy-employee-modal')->close();
+            $this->dispatch('toast', message: __('No se puede eliminar a un administrador de la empresa.'), type: 'error');
+            return;
+        }
+        $employee->delete();
+
+        $this->table_items = array_filter($this->table_items, fn($item) => $item['id'] !== $this->selected_employee_id);
+        $this->refreshTableData();
+        $this->dispatch('toast', message: __('Empleado eliminado correctamente.'), type: 'success');
+
+        Flux::modal('confirm-destroy-employee-modal')->close();
     }
 
     public function render()
