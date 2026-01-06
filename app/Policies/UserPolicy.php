6ebb28eba4d719bdbbcdb6c5fc27754ecabbf17d
sandra santos <sandrapatiho@gmail.com>
2026-01-05 23:44:35 -0600

    Refactor the roles enum for user policies

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,27 +2,28 @@
 
 namespace App\Policies;
 
+use App\Enums\RoleEnum;
 use App\Models\User;
 
 class UserPolicy
 {
     public function viewSystemOwner(User $user): bool
     {
-        return $user->hasRole(role:'propietario');
+        return $user->hasRole(role: RoleEnum::SYSTEM_OWNER->value);
     }
 
     public function viewCompanyAdmin(User $user): bool
     {
-        return $user->hasRole(role:'administrador');
+        return $user->hasRole(role: RoleEnum::COMPANY_ADMIN->value);
     }
 
     public function viewDepartmentManager(User $user): bool
     {
-        return $user->hasRole(role:'gerente');
+        return $user->hasRole(role: RoleEnum::DEPARTMENT_MANAGER->value);
     }
 
     public function viewEmployee(User $user): bool
     {
-        return $user->hasRole(role:'empleado');
+        return $user->hasRole(role:RoleEnum::EMPLOYEE->value);
     }
 }
