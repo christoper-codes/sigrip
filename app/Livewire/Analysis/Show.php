f3deadb38c90a8dd6b55d59fffc19d3ade711e87
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-11 15:46:09 -0600

    Show applications in modal

--- app/Livewire/Analysis/Show.php
+++ app/Livewire/Analysis/Show.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Analysis;
 
 use App\Models\Application;
 use App\Models\Department;
+use Flux\Flux;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
@@ -16,6 +17,9 @@ class Show extends Component
     #[Validate(['required', 'int'])]
     public ?int $department = null;
 
+    #[Validate(['nullable', 'array'])]
+    public ?array $application = null;
+
     public function mount(): void
     {
         $this->departments = Department::where('company_id', Auth::user()->company?->id)
@@ -27,9 +31,11 @@ class Show extends Component
     {
         $this->validate();
         $this->applications = Application::where('executing_department_id', $this->department)
-            ->orderByDesc('created_at')
+            ->orderByDesc('start_date')
             ->get()
             ->toArray();
+
+        Flux::modal('select-application')->show();
     }
 
     public function render()
--- resources/views/livewire/analysis/show.blade.php
+++ resources/views/livewire/analysis/show.blade.php
@@ -14,4 +14,34 @@
         </div>
         <flux:button type="submit" variant="primary" class="mt-3">{{ __('Buscar aplicaciones') }}</flux:button>
    </form>
+
+   <flux:modal name="select-application" class="w-[90%] md:w-xl space-y-5">
+        <div>
+            <flux:heading size="lg">{{ __('Seleccione una aplicación') }}</flux:heading>
+            <flux:text class="mt-2">{{ __('Ver resultados detallados') }}</flux:text>
+        </div>
+        <div>
+            @if($applications)
+                <flux:radio.group label="Role">
+                    @foreach ($applications as $application)
+                        <flux:radio
+                            name="application"
+                            value="{{ $application['id'] }}"
+                            label="{{ ucfirst(str_replace('-', ' ', explode('-', $application['slug'], -1) ? implode('-', explode('-', $application['slug'], -1)) : $application['slug'])) }}"
+                            description="{{ $application['start_date'] . ' - ' . $application['expiration_date'] }}"
+                            wire:model="application"
+                        />
+                    @endforeach
+                </flux:radio.group>
+            @endif
+        </div>
+        <div class="flex justify-end items-center gap-2">
+            <flux:modal.close>
+                <flux:button>{{ __('Cancelar') }}</flux:button>
+            </flux:modal.close>
+            <flux:modal.close>
+                <flux:button variant="primary">{{ __('Buscar resultados') }}</flux:button>
+            </flux:modal.close>
+        </div>
+    </flux:modal>
 </div>
