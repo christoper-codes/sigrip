c501164f8a8e9671040f5ba5aee6ce97ff070550
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-11 22:08:59 -0600

    Refactor dateFormat helper accept both

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -4,8 +4,30 @@ use Carbon\Carbon;
 
 function dateFormat(string $date): string
 {
-     $carbon = Carbon::parse($date);
-     $carbon->locale('es');
+     $formats = [
+          'd/m/Y H:i',
+          'd/m/Y',
+          'Y-m-d H:i:s',
+          'Y-m-d H:i',
+          'Y-m-d',
+     ];
+     $carbon = null;
+     foreach ($formats as $format) {
+          try {
+               $carbon = Carbon::createFromFormat($format, $date);
+               break;
+          } catch (Exception $e) {
+                continue;
+          }
+     }
+     if (!$carbon) {
+          try {
+               $carbon = Carbon::parse($date);
+          } catch (Exception $e) {
+               return $date;
+          }
+     }
 
+     $carbon->locale('es');
      return $carbon->translatedFormat('l, d \d\e F \d\e Y, H:i');
 }
