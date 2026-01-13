df0632e7e6f9254fc9af830a8e8de97158c55b26
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-12 23:23:56 -0600

    Show employees applications

--- app/Livewire/Application/Employee.php
+++ app/Livewire/Application/Employee.php
@@ -2,15 +2,30 @@
 
 namespace App\Livewire\Application;
 
+use App\Livewire\Traits\Table;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Employee extends Component
 {
+    use Table;
+
     public ?array $applications = null;
 
     public function mount(): void
     {
+        $this->table_items = Auth::user()->applications()->get()->toArray() ?? [];
 
+        $this->search_fields = ['name'];
+        $this->headers = [
+            ['label' => __('Nombre')],
+            ['label' => __('Fecha de Inicio'), 'field' => 'start_date', 'sortable' => true],
+            ['label' => __('Fecha de Expiración'), 'field' => 'expiration_date', 'sortable' => true],
+            ['label' => __('Aplicar')],
+            ['label' => __('Se ha respondido')],
+            ['label' => __('Fecha de respuesta'), 'field' => 'response_date', 'sortable' => true],
+        ];
+         $this->refreshTableData();
     }
 
     public function render()
--- resources/views/livewire/application/employee.blade.php
+++ resources/views/livewire/application/employee.blade.php
@@ -1,3 +1,39 @@
 <div>
-
+    <x-appearance.livewiretable
+        :headers="$headers"
+        search_placeholder="{{ __('Nombre de la aplicación') }}"
+        :total_results="$total_results"
+        :current_page="$current_page"
+        :total_pages="$total_pages"
+        :paginated_items="$paginated_items"
+        :sort_field="$sort_field"
+        :sort_direction="$sort_direction"
+        >
+        <x-slot:table>
+            @foreach ($paginated_items as $application)
+                <tr>
+                    <td class="p-4">{{ ucfirst(str_replace('-', ' ', explode('-', $application['slug'], -1) ? implode('-', explode('-', $application['slug'], -1)) : $application['slug'])) }}</td>
+                    <td class="p-4">{{ $application['start_date'] }}</td>
+                    <td class="p-4">{{ $application['expiration_date'] }}</td>
+                    <td class="p-4">
+                        <flux:button href="{{ route('application.show', ['slug' => $application['slug']]) }}" icon="clipboard-document-list" class="border! border-primary! bg-primary/10!">{{ __('Aplicar') }}</flux:button>
+                    </td>
+                    <td class="p-4">
+                        @if($application['pivot']['is_active'])
+                            <flux:icon.x-circle />
+                        @else
+                            <flux:icon.check-circle />
+                        @endif
+                    </td>
+                    <td class="p-4">
+                        @if(! $application['pivot']['is_active'])
+                            {{ $application['pivot']['updated_at'] }}
+                        @else
+                            {{ __('Sin fecha') }}
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </x-slot:table>
+     </x-appearance.livewiretable>
 </div>
