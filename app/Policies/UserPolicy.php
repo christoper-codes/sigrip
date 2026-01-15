2366c10acd919bcb64df0deb3fa48cf6c7c1bb2f
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-15 16:18:51 -0600

    Refactor User Policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -7,23 +7,61 @@ use App\Models\User;
 
 class UserPolicy
 {
+    private function permission(array $roles, User $user): bool
+    {
+        return $user->userRoles()
+            ->whereIn('name', $roles)
+            ->exists();
+    }
+
     public function viewSystemOwner(User $user): bool
     {
-        return $user->hasRole(role: RoleEnum::SYSTEM_OWNER->value);
+        if($user->hasRole(role: RoleEnum::SYSTEM_OWNER->value)){
+            return true;
+        }
+
+        return false;
     }
 
     public function viewCompanyAdmin(User $user): bool
     {
-        return $user->hasRole(role: RoleEnum::COMPANY_ADMIN->value);
+        if($user->hasRole(role: RoleEnum::COMPANY_ADMIN->value)){
+            return true;
+        }
+
+        $roles = [
+            RoleEnum::SYSTEM_OWNER->value,
+        ];
+
+        return $this->permission(roles: $roles, user: $user);
     }
 
     public function viewDepartmentManager(User $user): bool
     {
-        return $user->hasRole(role: RoleEnum::DEPARTMENT_MANAGER->value);
+        if($user->hasRole(role: RoleEnum::DEPARTMENT_MANAGER->value)){
+            return true;
+        }
+
+        $roles = [
+            RoleEnum::SYSTEM_OWNER->value,
+            RoleEnum::COMPANY_ADMIN->value,
+        ];
+
+        return $this->permission(roles: $roles, user: $user);
     }
 
     public function viewEmployee(User $user): bool
     {
-        return $user->hasRole(role:RoleEnum::EMPLOYEE->value);
+        if($user->hasRole(role: RoleEnum::EMPLOYEE->value)){
+            return true;
+        }
+
+        $roles = [
+            RoleEnum::SYSTEM_OWNER->value,
+            RoleEnum::COMPANY_ADMIN->value,
+            RoleEnum::DEPARTMENT_MANAGER->value,
+        ];
+
+        return $this->permission(roles: $roles, user: $user);
     }
 }
