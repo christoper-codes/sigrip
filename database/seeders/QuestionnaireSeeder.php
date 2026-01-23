770ca04ae2d25a24c01856acf1f315a5dbcdaaef
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-23 13:45:48 -0600

    Add nom035 questionnaire

--- database/seeders/QuestionnaireSeeder.php
+++ database/seeders/QuestionnaireSeeder.php
@@ -15,12 +15,13 @@ class QuestionnaireSeeder extends Seeder
         $monthly_scan = file_get_contents(database_path('seeders/data/monthly_scan_001.json'));
         $weekly_scan = file_get_contents(database_path('seeders/data/weekly_scan_001.json'));
         $honestly_test = file_get_contents(database_path('seeders/data/honestly_test_001.json'));
+        $nom035_1 = file_get_contents(database_path('seeders/data/nom035_01.json'));
 
         Questionnaire::create([
             'questionnaire_category_id' => 1,
             'organization_id' => 1,
-            'name' => 'Escaneo emocional mensual (NOM-035)',
-            'description' => 'Cuestionario para identificar y evaluar los factores de riesgo psicosocial en el entorno laboral, conforme a la NOM-035',
+            'name' => 'Escaneo emocional mensual',
+            'description' => 'Cuestionario para identificar y evaluar los factores de riesgo psicosocial en el entorno laboral',
             'metadata' => json_decode($monthly_scan, true),
             'is_base' => true,
             'is_active' => true,
@@ -29,8 +30,8 @@ class QuestionnaireSeeder extends Seeder
         Questionnaire::create([
             'questionnaire_category_id' => 2,
             'organization_id' => 1,
-            'name' => 'Escaneo emocional semanal (NOM-035)',
-            'description' => 'Cuestionario semanal para monitorear el bienestar emocional de los empleados, conforme a la NOM-035',
+            'name' => 'Escaneo emocional semanal',
+            'description' => 'Cuestionario semanal para monitorear el bienestar emocional de los empleados',
             'metadata' => json_decode($weekly_scan, true),
             'is_base' => true,
             'is_active' => true,
@@ -39,9 +40,19 @@ class QuestionnaireSeeder extends Seeder
         Questionnaire::create([
             'questionnaire_category_id' => 3,
             'organization_id' => 1,
-            'name' => 'Test de honestidad laboral (NOM-035)',
+            'name' => 'Test de honestidad laboral',
             'description' => 'Evaluación diseñada para medir la honestidad y la integridad de los empleados en el entorno laboral.',
             'metadata' => json_decode($honestly_test, true),
+            'is_base' => true,
+            'is_active' => true,
+        ]);
+
+        Questionnaire::create([
+            'questionnaire_category_id' => 1,
+            'organization_id' => 1,
+            'name' => 'Guía de Referencia I - NOM-035',
+            'description' => 'Identificación de trabajadores sujetos a acontecimientos traumáticos severos',
+            'metadata' => json_decode($nom035_1, true),
             'is_base' => true,
             'is_active' => true,
         ]);
