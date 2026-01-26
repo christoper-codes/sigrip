e2bc18fe74064274aabbb35800cea331d3060786
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-25 22:42:08 -0600

    Add nom035_2 questionnaire to seeder

--- database/seeders/QuestionnaireSeeder.php
+++ database/seeders/QuestionnaireSeeder.php
@@ -16,6 +16,7 @@ class QuestionnaireSeeder extends Seeder
         $weekly_scan = file_get_contents(database_path('seeders/data/weekly_scan_001.json'));
         $honestly_test = file_get_contents(database_path('seeders/data/honestly_test_001.json'));
         $nom035_1 = file_get_contents(database_path('seeders/data/nom035_01.json'));
+        $nom035_2 = file_get_contents(database_path('seeders/data/nom035_02.json'));
 
         Questionnaire::create([
             'questionnaire_category_id' => 1,
@@ -23,6 +24,16 @@ class QuestionnaireSeeder extends Seeder
             'name' => 'Guia de Referencia I - (NOM-035)',
             'description' => 'Identificación de trabajadores sujetos a acontecimientos traumáticos severos',
             'metadata' => json_decode($nom035_1, true),
+            'is_base' => true,
+            'is_active' => true,
+        ]);
+
+        Questionnaire::create([
+            'questionnaire_category_id' => 1,
+            'organization_id' => 1,
+            'name' => 'Guia de Referencia II - (NOM-035)',
+            'description' => 'Identificación y evaluación de los factores de riesgo psicosocial en el entorno laboral',
+            'metadata' => json_decode($nom035_2, true),
             'is_base' => true,
             'is_active' => true,
         ]);
