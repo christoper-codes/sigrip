b9bb4355a02338c14f27286f98cb43aa58ddafee
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-29 23:40:35 -0600

    Refactor  FinalScoreAction

--- app/Actions/Analysis/FinalScoreAction.php
+++ app/Actions/Analysis/FinalScoreAction.php
@@ -4,9 +4,22 @@ namespace App\Actions\Analysis;
 
 class FinalScoreAction
 {
-    public function execute(array $responses): int
+    public function execute(array $responses): array
     {
-        return collect($responses)
+        $final_score = collect($responses)
             ->sum(fn ($r) => (int) $r['value']);
+
+        $classification = match (true) {
+            $final_score < 20  => 'Nulo o despreciable',
+            $final_score < 45  => 'Bajo',
+            $final_score < 70  => 'Medio',
+            $final_score < 90  => 'Alto',
+            default            => 'Muy alto',
+        };
+
+        return [
+            'final_score'   => $final_score,
+            'classification'=> $classification,
+        ];
     }
 }
