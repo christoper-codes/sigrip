5607b3a09059cb7fae3e8b35ed1a377269667adc
Christoper Patiño <christopher.code.git@gmail.com>
2026-01-31 14:36:03 -0600

    Ass MainNom2Export

---
+++ app/Exports/Nom035/MainNom2Export.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exports\Nom035;
+
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class MainNom2Export implements FromArray, WithHeadings, WithStyles, WithColumnWidths
+{
+
+}
