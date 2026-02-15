ad4361576cd0da431d7d4bf4a8890159e994f881
Christoper Patiño <christopher.code.git@gmail.com>
2026-02-14 20:00:19 -0600

    Add welcome mail

---
+++ app/Mail/Welcome.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Welcome extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public string $company){}
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Bienvenido a NEURA',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.welcome',
+            with: [
+                'company' => $this->company,
+                'url' => config('app.url'),
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/mail/welcome.blade.php
@@ -0,0 +1,17 @@
+<x-mail::message>
+# Bienvenido a NEURA
+
+Hola **{{ $company }}** 👋
+
+Tu cuenta ya está lista para comenzar con el cumplimiento de la **NOM-035**.
+
+<x-mail::button :url="$url">
+Comenzar
+</x-mail::button>
+
+Si tienes dudas, nuestro equipo te acompaña durante todo el proceso.
+
+Gracias,<br>
+
+**Equipo Neura**
+</x-mail::message>
