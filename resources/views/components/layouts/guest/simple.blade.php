334673bb2367db6e0d712f7fc3f33f52c6ab7f67
Christoper Patiño <christopher.code.git@gmail.com>
2026-02-14 22:03:29 -0600

    Add whatsapp link component

--- resources/views/components/layouts/guest/simple.blade.php
+++ resources/views/components/layouts/guest/simple.blade.php
@@ -1,6 +1,7 @@
 <x-layouts.base.master>
     @include('partials.header')
     <main>
+        <x-links.whatsapp />
         {{ $slot }}
     </main>
     @include('partials.footer')
---
+++ resources/views/components/links/whatsapp.blade.php
@@ -0,0 +1,3 @@
+<div class="fixed bottom-4 right-2 md:bottom-12 md:right-7 z-30 flex items-center justify-center size-16 rounded-full hover:cursor-pointer">
+    <img src="/images/whatsapp.svg" alt="whatsapp logo" class="size-12 md:size-14">
+</div>
