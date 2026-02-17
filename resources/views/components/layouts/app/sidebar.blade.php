3c2c797f92df0e8322ecfb4263c6906918da909d
Christoper Patiño <christopher.code.git@gmail.com>
2026-02-16 21:54:49 -0600

    Refactor dark-variant

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -7,7 +7,7 @@
         <x-alert />
         <livewire:notifications.toast />
 
-        <flux:sidebar sticky collapsible stashable class="border-e overflow-hidden border-zinc-200 bg-dark dark:border-border dark:bg-card">
+        <flux:sidebar sticky collapsible stashable class="border-e overflow-hidden border-border bg-dark dark:bg-card">
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
                 <flux:sidebar.header>
                     <flux:sidebar.brand
