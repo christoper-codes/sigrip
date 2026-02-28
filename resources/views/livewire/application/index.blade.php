003ec3180acc7e8eb03e840945c63692644618ac
Christoper Patiño <christopher.code.git@gmail.com>
2026-02-28 16:22:41 -0600

    Remove department filter from application search form in index view

--- resources/views/livewire/application/index.blade.php
+++ resources/views/livewire/application/index.blade.php
@@ -1,16 +1,7 @@
 <div>
    <form wire:submit.prevent='searchApplications'>
         <div class="flex items-center gap-3">
-            <flux:field class="max-w-md w-full">
-                <flux:label>{{ __('Filtrar por departamento') }}</flux:label>
-                <flux:select class="!h-12" name="department" wire:model.live="department">
-                    <flux:select.option value="" >{{ __('Selecciona un departamento') }}</flux:select.option>
-                    @foreach ($departments as $department)
-                        <flux:select.option value="{{ $department['id'] }}">{{ $department['name'] }}</flux:select.option>)
-                    @endforeach
-                </flux:select>
-                <flux:error name="department" class="!mt-0"/>
-            </flux:field>
+
             <flux:field class="max-w-32 w-full">
                 <flux:label>{{ __('Total de registros') }}</flux:label>
                 <flux:select class="!h-12" name="items_per_page" wire:model.live="items_per_page">
