e70358502133452a91045f46ce9657554225498d
Christoper Patiño <christopher.code.git@gmail.com>
2026-03-03 19:46:17 -0600

    Refactor dashboard view

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -7,7 +7,7 @@
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-20 text-white lg:flex">
                 <div class="absolute inset-0 rounded-none m-5 bg-cover bg-center bg-no-repeat border border-neutral-900"
-                     style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)), url('/images/auth2.jpeg');">
+                     style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.9)), url('/images/auth2.jpeg');">
                 </div>
                 <a href="{{ route('home') }}" class="relative z-20 flex items-center" wire:navigate>
                     <x-app-logo />
