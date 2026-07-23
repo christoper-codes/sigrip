e31d41c0cd830b6a05d1c471698235efc798cc2a
christoper <christopher.code.git@gmail.com>
2026-07-22 18:13:20 -0600

    Remove unused background elements from multiple application views for cleaner design

--- resources/views/pages/app/application/show.blade.php
+++ resources/views/pages/app/application/show.blade.php
@@ -1,8 +1,6 @@
 <x-layouts.application :title="__('Applicación')">
     <section id="application-show" class="h-full!">
         <flux:main class="!max-w-[70rem] !w-full h-full! !mx-auto !px-0 !py-0 relative flex! flex-col! justify-between!">
-            <div class="absolute left-0 lg:hidden top-0 lg:-top-96 h-[280px] w-[100px] lg:h-[400px] lg:w-[300px] rounded-full blur-[100px] lg:blur-[150px] bg-primary/30"></div>
-            <div class="absolute hidden lg:block right-0 top-0 lg:-top-96 h-[280px] w-[100px] lg:h-[400px] lg:w-[300px] rounded-full blur-[100px] lg:blur-[150px] bg-primary/30"></div>
             <div>
                 <div class="flex items-center justify-between relative">
                     <a href="{{ route('home') }}" wire:navigate>
